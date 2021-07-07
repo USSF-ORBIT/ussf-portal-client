1ba3b0f903f2a8eb6009b25f4740ac76f461c978
Suzanne Rozier <suz@truss.works>
2021-07-07 11:28:43 -0500

    Add Head component

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,4 +3,10 @@ describe('The Home Page', () => {
     cy.visit('http://localhost:3000')
     cy.contains('USSF Portal')
   })
+
+  it('contains the expected meta data', () => {
+    cy.visit('http://localhost:3000')
+    cy.document()
+    cy.get('head title').should('contain', 'Space Force Portal')
+  })
 })
---
+++ layout/head.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+
+// TODO - description
+// TODO - title prop
+// TODO - canonical URL
+// TODO icons
+// TODO manifest?
+// TODO theming
+
+const USSFPortalHead = () => {
+  return (
+    <Head>
+      <meta charSet="utf-8" key="charset" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" key="edge" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+        key="viewport"
+      />
+
+      <title>Space Force Portal</title>
+    </Head>
+  )
+}
+
+export default USSFPortalHead
