0c30040520f41caab5e2b860ef8d7a0b83262fd4
Abigail Young <abbyoung@gmail.com>
2021-07-07 13:51:08 -0700

    chore: Set up USWDS Sass dependencies (#26)
    * Add USWDS dependencies and scss files
    
    In addition to the package itself, this PR adds scss support
    compatible with webpack4 for Storybook. This setup mirrors the
    ussf-portal-prototype.
    
    Fixes #14

---
+++ components/Logo/Logo.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+import Logo from './Logo'
+
+export default {
+  title: 'Components/Logo',
+  component: Logo,
+} as Meta
+
+export const DefaultLogo = () => <Logo />
--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './Logo.module.scss'
 
 const Logo = () => {
-  return <img src="/img/ussf-logo.svg" alt="USSF Logo" />
+  return <img src="/img/ussf-logo.svg" alt="USSF Logo" className={styles.default} />
 }
 
 export default Logo
