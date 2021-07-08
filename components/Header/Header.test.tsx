dc52744672f82389a80ec207ba5da6d5b58db553
Suzanne Rozier <suz@truss.works>
2021-07-08 10:02:08 -0500

    Add navigation

--- components/Header/Header.test.tsx
+++ components/Header/Header.test.tsx
@@ -3,6 +3,7 @@
  */
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import Header from './Header'
 
@@ -10,5 +11,57 @@ describe('Header component', () => {
   it('renders the Space Force logo', () => {
     render(<Header />)
     expect(screen.getByAltText('Space Force')).toBeInTheDocument()
+  })
+
+  it('renders the primary navigation links', () => {
+    render(<Header />)
+
+    const nav = screen.getByRole('navigation')
+    expect(nav).toBeInTheDocument()
+
+    // There are two "Home" links because the logo is also one
+    const homeLinks = screen.getAllByRole('link', { name: 'Home' })
+    expect(homeLinks).toHaveLength(2)
+    homeLinks.forEach((l) => {
+      expect(l).toBeInstanceOf(HTMLAnchorElement)
+    })
+
+    // Test for other links
+    expect(screen.getByRole('link', { name: 'News' })).toBeInstanceOf(
+      HTMLAnchorElement
+    )
+    expect(
+      screen.getByRole('link', { name: 'Training and education' })
+    ).toBeInstanceOf(HTMLAnchorElement)
+    expect(screen.getByRole('link', { name: 'About us' })).toBeInstanceOf(
+      HTMLAnchorElement
+    )
+  })
+
+  it('renders the Service Portal subnav', () => {
+    render(<Header />)
+
+    const toggleButton = screen.getByRole('button', {
+      name: 'Your Service Portal',
+    })
+    expect(toggleButton).toBeInstanceOf(HTMLButtonElement)
+
+    userEvent.click(toggleButton)
+
+    expect(screen.getByRole('link', { name: 'Army' })).toBeInstanceOf(
+      HTMLAnchorElement
+    )
+    expect(screen.getByRole('link', { name: 'Navy' })).toBeInstanceOf(
+      HTMLAnchorElement
+    )
+    expect(screen.getByRole('link', { name: 'Air Force' })).toBeInstanceOf(
+      HTMLAnchorElement
+    )
+    expect(screen.getByRole('link', { name: 'Marines' })).toBeInstanceOf(
+      HTMLAnchorElement
+    )
+    expect(screen.getByRole('link', { name: 'Coast Guard' })).toBeInstanceOf(
+      HTMLAnchorElement
+    )
   })
 })
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,27 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Link from 'next/link'
 import {
   Header as USWDSHeader,
   Title,
   NavMenuButton,
+  PrimaryNav,
+  Menu,
+  NavDropDownButton,
 } from '@trussworks/react-uswds'
 
 // TODO - display current page title in sr-only
 // TODO - mobile menu expand/collapse
+// TODO - current page class
 
-const Header = () => (
-  <USWDSHeader basic>
-    <div className="usa-nav-container">
-      <div className="usa-navbar">
-        <Title>
-          {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-          <a href="/" title="Home" aria-label="Home">
-            <img src="/img/ussf-logo.svg" alt="Space Force" />
-          </a>
-        </Title>
-        <NavMenuButton label="Menu" />
+const Header = () => {
+  const [servicePortalNavOpen, setServicePortalNavOpen] = useState(false)
+
+  const navItems = [
+    { path: '/', label: 'Home' },
+    { path: '/news', label: 'News' },
+    { path: '/training-and-education', label: 'Training and education' },
+    { path: '/about-us', label: 'About us' },
+  ].map((i) => (
+    <Link href={i.path} key={i.path}>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a className="usa-nav__link">
+        <span>{i.label}</span>
+      </a>
+    </Link>
+  ))
+
+  const servicePortalItems = [
+    { path: 'https://www.hrcapps.army.mil/portal/', label: 'Army' },
+    { path: 'https://my.navy.mil/', label: 'Navy' },
+    { path: 'https://www.my.af.mil/', label: 'Air Force' },
+    { path: 'https://mol.tfs.usmc.mil/', label: 'Marines' },
+    { path: 'https://cgportal2.uscg.mil/', label: 'Coast Guard' },
+  ].map((i) => (
+    <Link href={i.path} key={i.path}>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a>{i.label}</a>
+    </Link>
+  ))
+
+  const handleToggleServicePortalMenu = () => {
+    setServicePortalNavOpen(!servicePortalNavOpen)
+  }
+
+  const servicePortalMenu = (
+    <>
+      <NavDropDownButton
+        label="Your Service Portal"
+        isOpen={servicePortalNavOpen}
+        menuId="basic-nav-section-one"
+        onToggle={handleToggleServicePortalMenu}
+      />
+      <Menu
+        items={servicePortalItems}
+        isOpen={servicePortalNavOpen}
+        id="basic-nav-section-one"
+      />
+    </>
+  )
+
+  navItems.push(servicePortalMenu)
+
+  return (
+    <USWDSHeader basic>
+      <div className="usa-nav-container">
+        <div className="usa-navbar">
+          <Title>
+            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+            <a href="/" title="Home" aria-label="Home">
+              <img
+                className="logo-img"
+                src="/img/ussf-logo.svg"
+                alt="Space Force"
+              />
+            </a>
+          </Title>
+          <NavMenuButton label="Menu" />
+        </div>
+
+        <PrimaryNav items={navItems} />
       </div>
-    </div>
-  </USWDSHeader>
-)
+    </USWDSHeader>
+  )
+}
 
 export default Header
