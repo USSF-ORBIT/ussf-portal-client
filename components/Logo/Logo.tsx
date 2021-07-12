9326cab02e43a61dc2ebe9bb4b435900e8423b9a
Suzanne Rozier <suz@truss.works>
2021-07-12 17:52:47 -0500

    Remove unused import

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from './Logo.module.scss'
 
 const Logo = () => {
-  return <img src="/img/ussf-logo.svg" alt="USSF Logo" className={styles.default} />
+  return (
+    <img src="/img/ussf-logo.svg" alt="USSF Logo" className={styles.default} />
+  )
 }
 
 export default Logo
