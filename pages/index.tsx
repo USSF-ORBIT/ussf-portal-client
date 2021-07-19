4fb362da23e06dfa6a3e85429d75f1aad3c6f591
Abigail Young <abbyoung@gmail.com>
2021-07-19 16:05:37 -0700

    Add CovidAlert component to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,10 @@
-// https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md#case-i-use-nextjs-and-im-getting-this-error-inside-of-links
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import Link from 'next/link'
-
-import Logo from 'components/Logo/Logo'
+import CovidSiteAlert from 'components/MVP/CovidSiteAlert/CovidSiteAlert'
 
 const Home = () => {
   return (
-    <>
-      <Logo />
-      USSF Portal
-      <Link href="/test-page">
-        <a>A test page</a>
-      </Link>
-    </>
+    <main id="main-content">
+      <CovidSiteAlert />
+    </main>
   )
 }
 
