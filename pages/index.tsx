751039a667bc1909bf492899486466b1dfdfdfd2
Abigail Young <abbyoung@gmail.com>
2021-07-19 15:22:08 -0700

    Update link on index to use LinkTo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md#case-i-use-nextjs-and-im-getting-this-error-inside-of-links
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import Link from 'next/link'
-
+import LinkTo from 'components/LinkTo/LinkTo'
 import Logo from 'components/Logo/Logo'
 
 const Home = () => {
@@ -9,9 +8,7 @@ const Home = () => {
     <>
       <Logo />
       USSF Portal
-      <Link href="/test-page">
-        <a>A test page</a>
-      </Link>
+      <LinkTo href="/test-page">A test page</LinkTo>
     </>
   )
 }
