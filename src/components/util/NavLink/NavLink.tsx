c9fc94e9f8303b87fd8f3704def7adbcdbd3a87e
Suzanne Rozier <suz@truss.works>
2021-07-21 17:09:51 -0500

    Working on NavLink component

---
+++ src/components/util/NavLink/NavLink.tsx
@@ -0,0 +1,39 @@
+// Inspired by https://stackoverflow.com/questions/60324081/adding-active-class-for-nav-link-in-next-js
+import React from 'react'
+import { useRouter } from 'next/router'
+import classnames from 'classnames'
+import { pathToRegexp } from 'path-to-regexp'
+
+import LinkTo from '../LinkTo/LinkTo'
+import type { PropTypes as LinkToProps } from '../LinkTo/LinkTo'
+
+type NavLinkProps = LinkToProps & {
+  exact?: boolean
+  activeClass?: string
+}
+
+const NavLink = ({
+  className,
+  exact,
+  activeClass = 'usa-current',
+  ...otherProps
+}: NavLinkProps) => {
+  const { href, as } = otherProps
+  const { asPath } = useRouter()
+
+  // TODO - handle other types? UrlObject { pathname: string }
+  const linkPath = (as || href) as string
+
+  const isActive = pathToRegexp(linkPath, [], {
+    sensitive: true,
+    end: !!exact,
+  }).test(asPath)
+
+  const classes = classnames(className, {
+    [activeClass]: isActive,
+  })
+
+  return <LinkTo {...otherProps} className={classes} />
+}
+
+export default NavLink
