6e723ef2220f9dcd2d137f1d881f7d2d4eaf3804
Suzanne Rozier <suz@truss.works>
2021-07-26 11:40:12 -0500

    feat: Accomplishments page (#46)
    * Content on About page
    
    * Styling About page
    
    * Add tests for About page
    
    * Add Cypress test for About page
    
    * Start Accomplishments page
    
    * Styling accomplishments list
    
    * Add tests

---
+++ src/__tests__/pages/about-us/accomplishments.test.tsx
@@ -0,0 +1,30 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react'
+
+import Accomplishments from 'pages/about-us/accomplishments'
+
+describe('Accomplishments page', () => {
+  beforeEach(() => {
+    render(<Accomplishments />)
+  })
+
+  it('renders the page title', () => {
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Things we’re proud of'
+    )
+  })
+
+  it.each([
+    [
+      'October 20, 2020',
+      'June, 2020',
+      'March 26, 2020',
+      'December 20th, 2019',
+      'February 19, 2019',
+    ],
+  ])('renders the %s item', (header) => {
+    expect(screen.getByRole('heading', { name: header })).toBeInTheDocument()
+  })
+})
