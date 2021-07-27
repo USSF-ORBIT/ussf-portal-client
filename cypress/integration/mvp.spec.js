147ddcde4ec8786ad9768447130b22d26e54ffbf
Suzanne Rozier <suz@truss.works>
2021-07-27 15:01:24 -0500

    feat: News page (#52)
    * Content on About page
    
    * Styling About page
    
    * Add tests for About page
    
    * Add Cypress test for About page
    
    * Start Accomplishments page
    
    * Styling accomplishments list
    
    * Add tests
    
    * Start News page
    
    * Load RSS articles
    
    * Style news page
    
    * Tests for News page
    
    * Fix &amp; encoding in news items
    
    * Test coverage

--- cypress/integration/mvp.spec.js
+++ cypress/integration/mvp.spec.js
@@ -8,6 +8,12 @@ describe('The MVP site', () => {
     cy.contains('USSF Portal')
   })
 
+  it('can navigate to the News page', () => {
+    cy.contains('News').click()
+    cy.url().should('contain', '/news')
+    cy.contains('What’s New')
+  })
+
   it('can navigate to the About Us page', () => {
     cy.contains('About us').click()
     cy.url().should('contain', '/about-us')
@@ -20,5 +26,19 @@ describe('The MVP site', () => {
     cy.contains('Our accomplishments').click()
     cy.url().should('contain', '/about-us/accomplishments')
     cy.contains('Things we’re proud of')
+  })
+
+  describe('the News page', () => {
+    beforeEach(() => {
+      cy.intercept(
+        'https://www.spaceforce.mil/DesktopModules/ArticleCS/RSS.ashx?ContentType=1&Site=1060&max=10'
+      ).as('getNewsRSS')
+
+      cy.visit('/news')
+    })
+
+    it('loads news articles from an RSS feed', () => {
+      cy.wait(['@getNewsRSS'])
+    })
   })
 })
