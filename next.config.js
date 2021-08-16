e5d1d86b6b2b75d3b13d6deeb33d0a061410de0b
Abigail Young <abbyoung@gmail.com>
2021-08-16 15:39:10 -0700

    Add i18n support to fix a11y issue with default lang

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPlugins = require('next-compose-plugins')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-
-module.exports = withBundleAnalyzer({})
+module.exports = withPlugins(
+  [withBundleAnalyzer], // All next plugins go here
+  {
+    i18n: {
+      locales: ['en-US'],
+      defaultLocale: 'en-US',
+    },
+  }
+)
