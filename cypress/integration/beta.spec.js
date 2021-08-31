84b3e721cd080dee7b93c2f161f98bb261e08239
Abigail Young <abbyoung@gmail.com>
2021-08-31 11:41:48 -0700

    wip testing cookies

--- cypress/integration/beta.spec.js
+++ cypress/integration/beta.spec.js
@@ -3,20 +3,13 @@ describe('the Beta site', () => {
   beforeEach(() => {
     cy.injectAxe()
   })
-
+  Cypress.Cookies.debug(true)
   it('joins the beta', () => {
     cy.visit('/joinbeta')
     cy.contains('Welcome to the Beta USSF Portal')
     cy.url().should('contain', '/')
-    Cypress.Cookies.preserveOnce('betaOptIn')
-  })
 
-  it('logs any a11y violations', () => {
-    cy.checkA11y(null, null, logging, { skipFailures: true })
-    Cypress.Cookies.preserveOnce('betaOptIn')
-  })
-
-  it('cannot access mvp pages', () => {
+    // Should not be able to access mvp pages
     cy.visit('/about-us', { failOnStatusCode: false })
     cy.request({ url: '/about-us', failOnStatusCode: false })
       .its('status')
@@ -27,17 +20,44 @@ describe('the Beta site', () => {
       .its('status')
       .should('equal', 404)
 
-    Cypress.Cookies.preserveOnce('betaOptIn')
-  })
-
-  it('leaves the beta', () => {
+    // Leave the beta
     cy.contains('Go Home').click()
     cy.contains('Welcome to the Beta USSF Portal')
-    cy.contains('Leave Beta').click()
-  })
+    cy.get('.usa-button').click()
 
-  it('returns to the MVP', () => {
+    cy.reload()
+
+    // Return to MVP
     cy.visit('/')
     cy.contains('Manage your life')
   })
+
+  it('logs any a11y violations', () => {
+    cy.checkA11y(null, null, logging, { skipFailures: true })
+  })
+
+  // it('cannot access mvp pages', () => {
+  //   cy.visit('/about-us', { failOnStatusCode: false })
+  //   cy.request({ url: '/about-us', failOnStatusCode: false })
+  //     .its('status')
+  //     .should('equal', 404)
+
+  //   cy.visit('/about-us/accomplishments', { failOnStatusCode: false })
+  //   cy.request({ url: '/about-us/accomplishments', failOnStatusCode: false })
+  //     .its('status')
+  //     .should('equal', 404)
+
+  //   Cypress.Cookies.preserveOnce('betaOptIn')
+  // })
+
+  // it('leaves the beta', () => {
+  //   cy.contains('Go Home').click()
+  //   cy.contains('Welcome to the Beta USSF Portal')
+  //   cy.contains('Leave Beta').click()
+  // })
+
+  // it('returns to the MVP', () => {
+  //   cy.visit('/')
+  //   cy.contains('Manage your life')
+  // })
 })
