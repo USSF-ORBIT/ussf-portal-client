37c6dfc0469e1b8a2c3a88effc0119df9980ed3c
Abigail Young <abbyoung@gmail.com>
2021-08-31 16:58:25 -0700

    Fix e2e test for static site

--- cypress/integration/beta.spec.js
+++ cypress/integration/beta.spec.js
@@ -1,28 +1,14 @@
 import logging from '../plugins/logging'
-describe('the Beta site', () => {
+describe('the static Beta site', () => {
   beforeEach(() => {
     cy.injectAxe()
   })
-  Cypress.Cookies.debug(true)
   it('joins the beta', () => {
     cy.visit('/joinbeta')
     cy.contains('Welcome to the Beta USSF Portal')
     cy.url().should('contain', '/')
 
-    // Should not be able to access mvp pages
-    cy.visit('/about-us', { failOnStatusCode: false })
-    cy.request({ url: '/about-us', failOnStatusCode: false })
-      .its('status')
-      .should('equal', 404)
-
-    cy.visit('/about-us/accomplishments', { failOnStatusCode: false })
-    cy.request({ url: '/about-us/accomplishments', failOnStatusCode: false })
-      .its('status')
-      .should('equal', 404)
-
     // Leave the beta
-    cy.contains('Go Home').click()
-    cy.contains('Welcome to the Beta USSF Portal')
     cy.get('.usa-button').click()
 
     cy.reload()
@@ -35,29 +21,4 @@ describe('the Beta site', () => {
   it('logs any a11y violations', () => {
     cy.checkA11y(null, null, logging, { skipFailures: true })
   })
-
-  // it('cannot access mvp pages', () => {
-  //   cy.visit('/about-us', { failOnStatusCode: false })
-  //   cy.request({ url: '/about-us', failOnStatusCode: false })
-  //     .its('status')
-  //     .should('equal', 404)
-
-  //   cy.visit('/about-us/accomplishments', { failOnStatusCode: false })
-  //   cy.request({ url: '/about-us/accomplishments', failOnStatusCode: false })
-  //     .its('status')
-  //     .should('equal', 404)
-
-  //   Cypress.Cookies.preserveOnce('betaOptIn')
-  // })
-
-  // it('leaves the beta', () => {
-  //   cy.contains('Go Home').click()
-  //   cy.contains('Welcome to the Beta USSF Portal')
-  //   cy.contains('Leave Beta').click()
-  // })
-
-  // it('returns to the MVP', () => {
-  //   cy.visit('/')
-  //   cy.contains('Manage your life')
-  // })
 })
