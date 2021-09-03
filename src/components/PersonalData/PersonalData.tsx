07292b05daccf1624e4b3cbc4cfec211cdc510b2
Abigail Young <abbyoung@gmail.com>
2021-09-03 16:34:50 -0700

    Add placeholder data component

--- src/components/PersonalData/PersonalData.tsx
+++ src/components/PersonalData/PersonalData.tsx
@@ -1,34 +1,31 @@
 import React from 'react'
-import { Meta } from '@storybook/react'
 import { GridContainer, Grid } from '@trussworks/react-uswds'
-import styles from './PersonalData'
+import styles from './PersonalData.module.scss'
 
 const PersonalData = () => {
-  const testContent = 'test content'
   return (
-    <>
-      <h2>Welcome, Sgt Snuffy</h2>
+    <div className={styles.personalData}>
       <GridContainer>
-        <Grid row gap={2}>
-          <Grid col={4}>
-            <dl>
-              <dt>Next PT Test:</dt>
-              <dd>In 67 days on 12 Dec 2021</dd>
-              <dt>Next Immunization:</dt>
-              <dd>Influenza due on 04 July 2021</dd>
-            </dl>
+        <h2>Welcome, Sgt Snuffy</h2>
+
+        <dl className="grid-row">
+          <Grid tablet={{ col: true }}>
+            <dt>Next PT Test:</dt>
+            <dd>In 67 days on 12 Dec 2021</dd>
+
+            <dt>Next Immunization:</dt>
+            <dd>Influenza due on 04 July 2021</dd>
           </Grid>
-          <Grid col={4}>
-            <dl>
-              <dt>Next EPR:</dt>
-              <dd>In 245 days on 12 July 2022</dd>
-              <dt>Current Leave Balance:</dt>
-              <dd>20.5 Days</dd>
-            </dl>
+          <Grid tablet={{ col: true }}>
+            <dt>Next EPR:</dt>
+            <dd>In 245 days on 12 July 2022</dd>
+
+            <dt>Current Leave Balance:</dt>
+            <dd>20.5 Days</dd>
           </Grid>
-        </Grid>
+        </dl>
       </GridContainer>
-    </>
+    </div>
   )
 }
 
