0d984e4d40e33ece3ac09700a765cc3416146e63
Abigail Young <abbyoung@gmail.com>
2021-09-03 10:58:44 -0700

    wip personal data placeholder

---
+++ src/components/PersonalData/PersonalData.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+import { CardGroup } from '@trussworks/react-uswds'
+import PersonalData from './PersonalData'
+
+export default {
+  title: 'Beta/Components/PersonalData',
+} as Meta
+
+export const PersonalDataPlaceholder = () => <PersonalData />
