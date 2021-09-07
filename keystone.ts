517aef4d156cbca5c20e0b7efbdef4ec8a12b608
Suzanne Rozier <suz@truss.works>
2021-09-07 17:12:37 -0500

    Move schema to separate file, write tests

--- keystone.ts
+++ keystone.ts
@@ -1,23 +1,6 @@
-import { createSchema, config, list } from '@keystone-next/keystone'
-import { text, relationship } from '@keystone-next/keystone/fields'
+import { config } from '@keystone-next/keystone'
 
-const Bookmark = list({
-  fields: {
-    url: text({
-      isRequired: true,
-    }),
-    label: text(),
-  },
-})
-
-const Collection = list({
-  fields: {
-    title: text({
-      isRequired: true,
-    }),
-    bookmarks: relationship({ ref: 'Bookmark', many: true }),
-  },
-})
+import schema from './src/cms/schema'
 
 export default config({
   db: { provider: 'sqlite', url: 'file:./cms.db' },
@@ -25,5 +8,5 @@ export default config({
     generateNextGraphqlAPI: true,
     generateNodeAPI: true,
   },
-  lists: createSchema({ Bookmark, Collection }),
+  lists: schema,
 })
