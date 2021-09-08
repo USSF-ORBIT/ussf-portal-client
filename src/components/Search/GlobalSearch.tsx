d3f752d098ac10030f7c928d91595481c41745fa
Abigail Young <abbyoung@gmail.com>
2021-09-08 09:50:19 -0700

    Add dropdown for filter

--- src/components/Search/GlobalSearch.tsx
+++ src/components/Search/GlobalSearch.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import { Search } from '@trussworks/react-uswds'
 import styles from './GlobalSearch.module.scss'
 
 const GlobalSearch = () => {
-  const submitSearch = () => {
-    return
-  }
   return (
-    <Search
-      className={styles.globalSearch}
-      size="big"
-      onSubmit={submitSearch}
-    />
+    <form
+      className={`usa-search usa-search--big ${styles.globalSearch}`}
+      role="search">
+      <select className="usa-select usa-button" name="options" id="options">
+        <option value="all">All</option>
+        <option value="value1">Option 1</option>
+        <option value="value2">Option 2</option>
+        <option value="value3">Option 3</option>
+      </select>
+
+      <input
+        className="usa-input"
+        id="search-field-en-big"
+        type="search"
+        name="search"
+      />
+      <button className="usa-button" type="submit">
+        <span className="usa-search__submit-text">Search</span>
+      </button>
+    </form>
   )
 }
 
