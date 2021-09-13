b0bee3c780a5fbe92f5c304e0e1d57fd6b6a02c4
Suzanne Rozier <suz@truss.works>
2021-09-13 17:39:49 -0500

    feat: Sites & Applications page (#220)
    * PageNav component
    
    * Add abbreviated version of Logo
    
    * Add Header component
    
    * Style header
    
    * Add component tests
    
    * More tests
    
    * Footer component
    
    * Create layout for beta pages
    
    * Make layout mobile friendly
    
    * Move CSS fixes into beta layout
    
    * Add Search, styling
    
    * Fix Cypress test
    
    * Working on S&A page
    
    * Styling S&A
    
    * Add content, styling
    
    * Add tests
    
    * Add Cypress spec for Sites and Applications feature
    
    * Add mock Keystone API, fix test
    
    * Open Bookmark links in new window
    
    * Display external link icon on Bookmarks
    
    * Remove external link icon
    
    * Resolve linting errors
    
    * Up test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,16 +4,17 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
+  transformIgnorePatterns: ['node_modules/(?!(.keystone)/)'],
   moduleNameMapper: {
     '\\.(scss|sass|css)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
-    '__mocks__/(.*)': '<rootDir>/src/__mocks__/$1',
-    'components/(.*)': '<rootDir>/src/components/$1',
-    'hooks/(.*)': '<rootDir>/src/hooks/$1',
-    'layout/(.*)': '<rootDir>/src/layout/$1',
-    'pages/(.*)': '<rootDir>/src/pages/$1',
-    'stores/(.*)': '<rootDir>/src/stores/$1',
+    '^__mocks__/(.*)': '<rootDir>/src/__mocks__/$1',
+    '^components/(.*)': '<rootDir>/src/components/$1',
+    '^hooks/(.*)': '<rootDir>/src/hooks/$1',
+    '^layout/(.*)': '<rootDir>/src/layout/$1',
+    '^pages/(.*)': '<rootDir>/src/pages/$1',
+    '^stores/(.*)': '<rootDir>/src/stores/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   coverageThreshold: {
