4f3a9df33f443ebf782b55fcb00e901d8ac7bb2e
Abigail Young <abbyoung@gmail.com>
2021-09-21 12:56:05 -0700

    Seed cache with example collection and bookmarks

--- src/components/SeedCache.tsx
+++ src/components/SeedCache.tsx
@@ -2,77 +2,58 @@ import React, { useEffect } from 'react'
 import { v4 } from 'uuid'
 import { Collection } from 'types/index'
 import { useAddCollectionMutation } from 'operations/mutations/addCollection'
-import { useRemoveBookmarkMutation } from 'operations/mutations/removeBookmark'
 import { useCollectionsQuery } from 'operations/queries/getCollections'
-import { useAddBookmarkMutation } from 'operations/mutations/addBookmark'
-import { useRemoveCollectionMutation } from 'operations/mutations/removeCollection'
+
 const SeedCache = () => {
-  // const [addCollection] = useAddCollectionMutation()
-  // const [addBookmark] = useAddBookmarkMutation()
-  const testCollectionId = '32cb181e-c118-4046-a1f5-344e898ac1a5'
-  const [removeCollection] = useRemoveCollectionMutation()
-  // const { data } = useCollectionsQuery(testCollectionId)
+  const { data } = useCollectionsQuery()
 
-  // console.log('Use Query to get one collections')
-  // console.log(data)
+  if (data !== undefined && data.collections.length > 0) return null
 
-  // const [removeBookmark] = useRemoveBookmarkMutation()
+  const [addCollection] = useAddCollectionMutation()
 
   // Create a default collection to add to the cache for testing
-  const searchCollection: Collection = {
+  const exampleCollection: Collection = {
     id: v4(),
-    title: 'Search Engines',
+    title: 'Example Collection',
     bookmarks: [
       {
         id: v4(),
         url: 'https://google.com',
-        label: 'Google',
-        description: 'Google home page',
+        label: 'Webmail',
       },
       {
         id: v4(),
-        url: 'https://bing.com',
-        label: 'Bing',
-        description: 'Bing home page',
+        url: 'https://mypay.dfas.mil/#/',
+        label: 'MyPay',
       },
       {
         id: v4(),
-        url: 'https://duckduckgo.com',
-        label: 'Duck Duck Go',
-        description: 'Quack',
+        url: 'https://afpcsecure.us.af.mil/PKI/MainMenu1.aspx',
+        label: 'vMPF',
+      },
+      {
+        id: v4(),
+        url: 'https://leave.af.mil/profile',
+        label: 'LeaveWeb',
+      },
+      {
+        id: v4(),
+        url: 'https://www.e-publishing.af.mil/',
+        label: 'e-Publications',
       },
     ],
   }
 
   useEffect(() => {
-    // addCollection({
-    //   variables: {
-    //     id: searchCollection.id,
-    //     title: searchCollection.title,
-    //     bookmarks: searchCollection.bookmarks,
-    //   },
-    // })
-    // addBookmark({
-    //   variables: {
-    //     url: 'tennis.com',
-    //     label: 'Table Tennis',
-    //     description: 'A website for Tennis enthusiasts',
-    //     collectionId: testCollectionId,
-    //   },
-    // })
-    // removeBookmark({
-    //   variables: {
-    //     id: '6b7a73f3-781d-4d64-b06e-72fbe6955b36',
-    //     collectionId: testCollectionId,
-    //   },
-    // })
-    // removeCollection({
-    //   variables: {
-    //     id: '8be444d6-38cd-4745-bdfe-fdb103c16003',
-    //   },
-    // })
+    addCollection({
+      variables: {
+        id: exampleCollection.id,
+        title: exampleCollection.title,
+        bookmarks: exampleCollection.bookmarks,
+      },
+    })
   }, [])
-  return <></>
+  return null
 }
 
 export default SeedCache
