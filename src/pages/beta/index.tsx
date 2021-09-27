63429ea1275fd967df36ed6fb997dae8b1aeb1a8
Suzanne Rozier <suz@truss.works>
2021-09-27 16:58:07 -0500

    Eslint cleanup

--- src/pages/beta/index.tsx
+++ src/pages/beta/index.tsx
@@ -1,17 +1,8 @@
 import { ReactNode } from 'react'
-import { Alert, Button } from '@trussworks/react-uswds'
 import Layout from 'layout/Beta/DefaultLayout/DefaultLayout'
-import { useBetaContext } from 'stores/betaContext'
 import MySpace from 'components/MySpace/MySpace'
 
-const Home = () => {
-  const { leaveBeta } = useBetaContext()
-  return (
-    <>
-      <MySpace />
-    </>
-  )
-}
+const Home = () => <MySpace />
 
 export default Home
 
