dbcec2da694abe6210ea3cd228bad195c5127a6a
Suzanne Rozier <suz@truss.works>
2021-10-01 11:19:17 -0500

    Add modal root portal

---
+++ src/components/util/ModalPortal.tsx
@@ -0,0 +1,16 @@
+import { useRef, useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+const ModalPortal = ({ children }: { children: React.ReactNode }) => {
+  const ref = useRef<Element | null>()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    ref.current = document.querySelector('#modal-root')
+    setMounted(true)
+  }, [])
+
+  return mounted && ref.current ? createPortal(children, ref.current) : null
+}
+
+export default ModalPortal
