f7d006591c6e5bce803af75c814ebdb3d3676a2c
Suzanne Rozier <suz@truss.works>
2021-10-05 10:42:05 -0500

    Fix tests, split out RemovableBookmark component

--- src/components/CustomCollection/CustomCollection.test.tsx
+++ src/components/CustomCollection/CustomCollection.test.tsx
@@ -2,12 +2,12 @@
  * @jest-environment jsdom
  */
 
-import { act, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 import { v4 } from 'uuid'
 
-import CustomCollection, { RemovableBookmark } from './CustomCollection'
+import CustomCollection from './CustomCollection'
 
 const exampleCollection = {
   id: v4(),
@@ -59,13 +59,40 @@ describe('CustomCollection component', () => {
   })
 
   it('renders an Add Link toggleable form', () => {
-    const mockAddLink = jest.fn()
     render(
       <CustomCollection
         {...exampleCollection}
         handleRemoveBookmark={jest.fn()}
-        handleAddBookmark={mockAddLink}
+        handleAddBookmark={jest.fn()}
       />
+    )
+
+    const toggleFormButton = screen.getByRole('button', { name: '+ Add link' })
+    expect(toggleFormButton).toBeInTheDocument()
+
+    userEvent.click(toggleFormButton)
+    const urlInput = screen.getByLabelText('URL')
+    expect(urlInput).toBeInTheDocument()
+    expect(urlInput).toBeInvalid()
+
+    userEvent.type(urlInput, 'example')
+    expect(urlInput).toBeInvalid()
+    userEvent.type(urlInput, 'http://www.example.com')
+    expect(urlInput).toBeValid()
+  })
+
+  it('entering a new link opens the modal', () => {
+    const mockAddLink = jest.fn()
+    render(
+      <>
+        <CustomCollection
+          {...exampleCollection}
+          handleRemoveBookmark={jest.fn()}
+          handleAddBookmark={mockAddLink}
+        />
+        <div id="modal-root" />
+      </>,
+      { container: document.body }
     )
 
     const toggleFormButton = screen.getByRole('button', { name: '+ Add link' })
@@ -82,104 +109,17 @@ describe('CustomCollection component', () => {
     expect(urlInput).toBeValid()
 
     userEvent.click(screen.getByRole('button', { name: 'Add site' }))
+
+    // Open modal
+    expect(screen.getByRole('dialog')).toBeVisible()
+
+    const labelInput = screen.getByLabelText('Label')
+    expect(labelInput).toBeInvalid()
+    userEvent.type(labelInput, 'My Custom Link')
+    expect(labelInput).toBeValid()
+
+    userEvent.click(screen.getByRole('button', { name: 'Save link name' }))
+
     expect(mockAddLink).toHaveBeenCalled()
-  })
-})
-
-describe('RemovableBookmark component', () => {
-  afterEach(() => {
-    jest.useRealTimers()
-  })
-
-  const testBookmark = exampleCollection.bookmarks[0]
-
-  it('renders a bookmark with a delete handler', () => {
-    render(
-      <RemovableBookmark bookmark={testBookmark} handleRemove={jest.fn()} />
-    )
-
-    expect(screen.getByRole('link')).toHaveTextContent(testBookmark.label)
-    expect(
-      screen.getByRole('button', { name: 'Remove this bookmark' })
-    ).toBeInTheDocument()
-  })
-
-  it('renders the bookmark URL if there is no label', () => {
-    const testBookmarkNoLabel = {
-      id: v4(),
-      url: 'https://example.com',
-    }
-
-    render(
-      <RemovableBookmark
-        bookmark={testBookmarkNoLabel}
-        handleRemove={jest.fn()}
-      />
-    )
-
-    expect(screen.getByRole('link')).toHaveTextContent(testBookmarkNoLabel.url)
-  })
-
-  it('allows the delete handler to be undone', async () => {
-    jest.useFakeTimers()
-
-    const mockHandleRemove = jest.fn()
-
-    render(
-      <RemovableBookmark
-        bookmark={testBookmark}
-        handleRemove={mockHandleRemove}
-      />
-    )
-
-    const deleteButton = screen.getByRole('button', {
-      name: 'Remove this bookmark',
-    })
-    userEvent.click(deleteButton)
-
-    const undoButton = screen.getByRole('button', { name: 'Undo remove' })
-    expect(undoButton).toBeInTheDocument()
-    expect(deleteButton).not.toBeInTheDocument()
-    userEvent.click(undoButton)
-
-    expect(screen.getByRole('link')).toHaveTextContent(testBookmark.label)
-    expect(undoButton).not.toBeInTheDocument()
-    expect(
-      screen.getByRole('button', {
-        name: 'Remove this bookmark',
-      })
-    ).toBeInTheDocument()
-
-    act(() => {
-      jest.runAllTimers()
-    })
-
-    expect(mockHandleRemove).not.toHaveBeenCalled()
-  })
-
-  it('handles the delete button if not undone', () => {
-    jest.useFakeTimers()
-
-    const mockHandleRemove = jest.fn()
-
-    render(
-      <RemovableBookmark
-        bookmark={testBookmark}
-        handleRemove={mockHandleRemove}
-      />
-    )
-
-    const deleteButton = screen.getByRole('button', {
-      name: 'Remove this bookmark',
-    })
-    userEvent.click(deleteButton)
-
-    const undoButton = screen.getByRole('button', { name: 'Undo remove' })
-    expect(undoButton).toBeInTheDocument()
-    expect(deleteButton).not.toBeInTheDocument()
-
-    jest.runAllTimers()
-
-    expect(mockHandleRemove).toHaveBeenCalled()
   })
 })
