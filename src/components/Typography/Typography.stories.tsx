9e4cb25795213ed852f1d77fcd292441eefe985b
Mallory Robertson <mallory@truss.works>
2021-10-07 11:06:00 -0700

    Added typography to storybok

---
+++ src/components/Typography/Typography.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+import Typography from './Typography'
+
+export default {
+  title: 'Components/Typography',
+  component: Typography,
+} as Meta
+
+export const DefaultTypograpy = () => <Typography />
+
+//move const Typography into this file
---
+++ src/components/Typography/Typography.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+import styles from './Typography.module.scss'
+
+const Typography = () => {
+  return (
+    <div className="">
+      <h1 className="">Typography</h1>
+      <p className="">Typeface: Name, Trebuchet</p>
+      <table className={styles.typeTable}>
+        <tr>
+          <td>
+            <h6>42px / 2.625rem / 0</h6>
+          </td>
+          <td>
+            <h1>h1: The quick brown fox jumped over the lazy dog.</h1>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h6>32px / 2rem / 0</h6>
+          </td>
+          <td>
+            <h2 className="">
+              h2: The quick brown fox jumped over the lazy dog.
+            </h2>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h6 className="">22px / 1.375rem / 0</h6>
+          </td>
+          <td>
+            <h3 className="">
+              h3: The quick brown fox jumped over the lazy dog.
+            </h3>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h6 className="">16px / 1rem / 0</h6>
+          </td>
+          <td>
+            <h4 className="">
+              h4: The quick brown fox jumped over the lazy dog.
+            </h4>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h6 className="">15px / 0.9375rem / 0</h6>
+          </td>
+          <td>
+            <h5 className="">
+              h5: The quick brown fox jumped over the lazy dog.
+            </h5>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h6 className="">13px / 0.8125rem / 0</h6>
+          </td>
+          <td>
+            <h6 className="">
+              h6: The quick brown fox jumped over the lazy dog.
+            </h6>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h6 className="">16px / 1rem / 0</h6>
+          </td>
+          <td>
+            <p className="">
+              body1: The quick brown fox jumped over the lazy dog.
+            </p>
+          </td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default Typography
