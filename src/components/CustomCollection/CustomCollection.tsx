4e0f5de2451e60008c193f2738badce36b0df307
Suzanne Rozier <suz@truss.works>
2021-11-01 13:54:19 -0500

    Reset input validity when selecting an option

--- src/components/CustomCollection/CustomCollection.tsx
+++ src/components/CustomCollection/CustomCollection.tsx
@@ -96,11 +96,13 @@ const CustomCollection = ({
     const existingLink = value && selectedExistingLink(value)
     const enteredCustomLink = urlOptions.length !== bookmarkOptions.length
 
-    // console.log('select option', value)
-
     if (existingLink && enteredCustomLink) {
       // Remove entered custom link
       urlOptions.pop()
+      // Reset input validation
+      const inputEl = document.getElementById('bookmarkUrl') as HTMLInputElement
+      inputEl?.setCustomValidity('')
+      inputEl?.reportValidity()
     }
 
     urlInputValue.current = value || ''
@@ -108,8 +110,6 @@ const CustomCollection = ({
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
-
-    // console.log('INPUT CHANGE', value)
 
     if (value) {
       if (urlOptions.length === bookmarkOptions.length) {
