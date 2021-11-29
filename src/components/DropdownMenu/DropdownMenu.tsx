01dfdc0c3b03e3e345e20528d1cacd30aba60898
Suzanne Rozier <suz@truss.works>
2021-11-29 15:17:33 -0600

    feat: Apply default theme to beta pages & components (#374)
    * Add color swatches to Storybook
    
    * Add font face
    
    * SFDS
    
    * Link colors
    
    * Update header/footer
    
    * Add sfds class to beta component stories
    
    * Only use Sharp Sans for headers
    
    * Use theme colors
    
    * Style side navigation
    
    * Create PageHeader component
    
    * Styling PageHeader, Search
    
    * Style tweaks
    
    * Minor cleanup
    
    * Update content of PersonalData
    
    * Add disabled Search
    
    * Add PageHeader background image
    
    * Add buttons, hover states
    
    * Change focus color
    
    * Selection color
    
    * Resolve eslint warning
    
    * Misc minor color fixes
    
    * Design feedback
    
    * Add align prop to Dropdown component
    
    * Fix tests

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import classnames from 'classnames'
 import styles from 'components/DropdownMenu/DropdownMenu.module.scss'
+
+type DropdownMenuAlignment = 'left' | 'right' | 'center'
 
 type PropTypes = {
   toggleEl: React.ReactNode
   children: React.ReactNode
   isActive: boolean
+  align?: DropdownMenuAlignment
   dropdownRef?: React.RefObject<HTMLDivElement>
 }
+
 const DropdownMenu = ({
   children,
   isActive,
+  align,
   dropdownRef,
   toggleEl,
 }: PropTypes) => {
+  const menuClasses = classnames(styles.dropdownMenu, {
+    [styles.left]: align === 'left',
+    [styles.right]: align === 'right',
+    [styles.center]: align === 'center',
+  })
+
   return (
     // Ref needs to encompass the menu trigger in order
     // to properly track inside/outside click for open/close
@@ -20,7 +32,7 @@ const DropdownMenu = ({
       {toggleEl}
 
       {isActive && (
-        <div className={styles.dropdownMenu}>
+        <div className={menuClasses}>
           <ol>
             {Array.isArray(children) ? (
               children.map((child, i) => <li key={`dropdown${i}`}>{child}</li>)
