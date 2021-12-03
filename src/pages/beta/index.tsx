673bec97b95ed9ad2a17d90be764cfce855afc09
Suzanne Rozier <suz@truss.works>
2021-12-03 14:15:58 -0600

    Load MySpace bookmarks using API

--- src/pages/beta/index.tsx
+++ src/pages/beta/index.tsx
@@ -1,6 +1,6 @@
+import { useState, useEffect } from 'react'
 import { InferGetServerSidePropsType } from 'next'
-
-import { query } from '.keystone/api'
+import axios, { AxiosResponse } from 'axios'
 
 import type { BookmarkRecords } from 'types/index'
 import MySpace from 'components/MySpace/MySpace'
@@ -9,10 +9,25 @@ import { useUser } from 'hooks/useUser'
 import { withBetaLayout } from 'layout/Beta/DefaultLayout/DefaultLayout'
 
 const Home = ({
-  bookmarks,
   user,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   useUser(user)
+  const [bookmarks, setBookmarks] = useState<BookmarkRecords>([])
+
+  useEffect(() => {
+    const loadBookmarks = async () => {
+      try {
+        const response: AxiosResponse<{ bookmarks: BookmarkRecords }> =
+          await axios.get('/api/bookmarks')
+        setBookmarks(response.data.bookmarks)
+      } catch (e) {
+        console.error('error getting bookmarks', e)
+      }
+    }
+
+    loadBookmarks()
+  }, [])
+
   return <MySpace bookmarks={bookmarks} />
 }
 
@@ -20,11 +35,4 @@ export default Home
 
 Home.getLayout = withBetaLayout
 
-export const getServerSideProps = requireAuth(async () => {
-  const bookmarks: BookmarkRecords = (await query.Bookmark.findMany({
-    query: 'id url label',
-    orderBy: [{ label: 'asc' }],
-  })) as BookmarkRecords
-
-  return { props: { bookmarks } }
-})
+export const getServerSideProps = requireAuth()
