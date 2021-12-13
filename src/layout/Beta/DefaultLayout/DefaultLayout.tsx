13040058134c3177495a5bde5c8ebc946e7c075d
Suzanne Rozier <suz@truss.works>
2021-12-13 17:24:07 -0600

    Beta banner, feedback copy

--- src/layout/Beta/DefaultLayout/DefaultLayout.tsx
+++ src/layout/Beta/DefaultLayout/DefaultLayout.tsx
@@ -3,6 +3,7 @@ import { GovBanner, GridContainer, Grid } from '@trussworks/react-uswds'
 
 import styles from './DefaultLayout.module.scss'
 
+import BetaBanner from 'components/BetaBanner/BetaBanner'
 import Header from 'components/Header/Header'
 import PersonalData from 'components/PersonalData/PersonalData'
 import PageHeader from 'components/PageHeader/PageHeader'
@@ -30,6 +31,7 @@ const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
       </a>
       <div className={`${styles.siteContainer} sfds`}>
         <GovBanner tld=".mil" />
+        <BetaBanner />
         <Header />
         <main id="main-content">
           <PageHeader disableSearch>
