0486002dc6874992d1fd5729cc2cd632dc1f7365
Suzanne Rozier <suz@truss.works>
2021-12-22 16:43:30 -0600

    Use env vars for Matomo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -168,12 +168,14 @@ const USSFPortalApp = ({
             {`console.log('INIT Matomo tracker');
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['setDocumentTitle', document.domain + '/' + document.title]);
+  _paq.push(['setDoNotTrack', true])
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//localhost/";
+    var u="//${process.env.NEXT_PUBLIC_MATOMO_DOMAIN}/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
+    _paq.push(['setSiteId', '${process.env.NEXT_PUBLIC_MATOMO_SITE_ID}']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
