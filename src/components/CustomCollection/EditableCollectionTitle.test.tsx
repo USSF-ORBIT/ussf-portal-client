277f8b6d49654af853ecd89888acc12b80f8aa1c
Abigail Young <abbyoung@gmail.com>
2022-01-27 18:27:27 -0800

    updating tests

--- src/components/CustomCollection/EditableCollectionTitle.test.tsx
+++ src/components/CustomCollection/EditableCollectionTitle.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 
@@ -15,231 +15,76 @@ describe('EditableCollectionTitle component', () => {
         collectionId="testCollection123"
         text="Test Collection"
         onSave={jest.fn()}
+        onCancel={jest.fn()}
         onDelete={jest.fn()}
+        isEditing={false}
       />
     )
 
     expect(
-      screen.getByRole('button', {
-        name: 'Edit Test Collection collection title',
-      })
+      screen.getByRole('heading', { level: 3, name: 'Test Collection' })
     ).toBeInTheDocument()
   })
 
-  it('renders in edit mode if no title is passed in', () => {
-    render(
-      <EditableCollectionTitle
-        collectionId="testCollection123"
-        text=""
-        onSave={jest.fn()}
-        onDelete={jest.fn()}
-      />
-    )
-
-    expect(screen.getByLabelText('Collection Title')).toBeInTheDocument()
-    expect(screen.getByRole('textbox')).toHaveFocus()
-  })
-
-  it('renders a textarea when edit button is clicked', () => {
+  it('renders a form to edit title if isEditing is true', () => {
     render(
       <EditableCollectionTitle
         collectionId="testCollection123"
         text="Test Collection"
         onSave={jest.fn()}
+        onCancel={jest.fn()}
         onDelete={jest.fn()}
+        isEditing={true}
       />
     )
-    const editTitle = screen.getByRole('button', {
-      name: 'Edit Test Collection collection title',
-    })
+    expect(
+      screen.getByRole('textbox', { name: 'Collection Title' })
+    ).toBeInTheDocument()
 
-    userEvent.click(editTitle)
-    const input = screen.getByRole('textbox')
-    expect(input).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument()
 
     expect(
-      screen.queryByRole('button', {
-        name: 'Edit Test Collection collection title',
-      })
-    ).not.toBeInTheDocument()
+      screen.getByRole('button', { name: 'Save name' })
+    ).toBeInTheDocument()
   })
+})
 
-  it('enters edit mode when the title is focused by pressing enter', () => {
-    render(
-      <EditableCollectionTitle
-        collectionId="testCollection123"
-        text="Test Collection"
-        onSave={jest.fn()}
-        onDelete={jest.fn()}
-      />
-    )
-    const editTitle = screen.getByRole('button', {
-      name: 'Edit Test Collection collection title',
-    })
-    userEvent.tab()
-    expect(editTitle).toHaveFocus()
-    userEvent.keyboard('{enter}')
-    const input = screen.getByRole('textbox')
-    expect(input).toBeInTheDocument()
-    expect(
-      screen.queryByRole('button', {
-        name: 'Edit  Test Collection collection title',
-      })
-    ).not.toBeInTheDocument()
-  })
+it('saves the form', async () => {
+  const mockHandleOnSave = jest.fn()
+  render(
+    <EditableCollectionTitle
+      collectionId="testCollection123"
+      text="Test Collection"
+      onSave={mockHandleOnSave}
+      onCancel={jest.fn()}
+      onDelete={jest.fn()}
+      isEditing={true}
+    />
+  )
+  const input = screen.getByRole('textbox', { name: 'Collection Title' })
+  userEvent.type(input, 'New Title')
 
-  describe('saving a new value', () => {
-    it('saves on blur', () => {
-      const handleOnSave = jest.fn()
-      render(
-        <EditableCollectionTitle
-          collectionId="testCollection123"
-          text="Test Collection"
-          onSave={handleOnSave}
-          onDelete={jest.fn()}
-        />
-      )
-      const editTitle = screen.getByRole('button', {
-        name: 'Edit Test Collection collection title',
-      })
-      userEvent.click(editTitle)
-      const input = screen.getByRole('textbox')
-      userEvent.click(input)
-      userEvent.clear(input)
-      userEvent.type(input, 'Updated Title')
-      fireEvent.blur(input)
-      expect(handleOnSave).toHaveBeenCalled()
-      expect(
-        screen.getByRole('button', {
-          name: 'Edit Updated Title collection title',
-        })
-      ).toHaveTextContent('Updated Title')
-    })
+  const saveNameButton = screen.getByRole('button', { name: 'Save name' })
+  userEvent.click(saveNameButton)
+  // #TODO "errors" when clicking submit on form; expected behavior by jsdom
+  // <<Error: Not implemented: HTMLFormElement.prototype.submit>>
+  // Should I ignore the error?
+  expect(mockHandleOnSave).toBeCalledTimes(1)
+})
 
-    it('saves on enter', () => {
-      const handleOnSave = jest.fn()
-      render(
-        <EditableCollectionTitle
-          collectionId="testCollection123"
-          text="Test Collection"
-          onSave={handleOnSave}
-          onDelete={jest.fn()}
-        />
-      )
-      const editTitle = screen.getByRole('button', {
-        name: 'Edit Test Collection collection title',
-      })
-      userEvent.click(editTitle)
-      const input = screen.getByRole('textbox')
-      userEvent.click(input)
-      userEvent.clear(input)
-      userEvent.type(input, 'Updated Title{enter}')
-      expect(handleOnSave).toHaveBeenCalled()
-      expect(
-        screen.getByRole('button', {
-          name: 'Edit Updated Title collection title',
-        })
-      ).toHaveTextContent('Updated Title')
-    })
-
-    it('saves on esc', () => {
-      const handleOnSave = jest.fn()
-      render(
-        <EditableCollectionTitle
-          collectionId="testCollection123"
-          text="Test Collection"
-          onSave={handleOnSave}
-          onDelete={jest.fn()}
-        />
-      )
-      const editTitle = screen.getByRole('button', {
-        name: 'Edit Test Collection collection title',
-      })
-      userEvent.click(editTitle)
-      const input = screen.getByRole('textbox')
-      userEvent.click(input)
-      userEvent.clear(input)
-      userEvent.type(input, 'Updated Title{esc}')
-      expect(handleOnSave).toHaveBeenCalled()
-      expect(
-        screen.getByRole('button', {
-          name: 'Edit Updated Title collection title',
-        })
-      ).toHaveTextContent('Updated Title')
-    })
-
-    it('saves on tab', () => {
-      const handleOnSave = jest.fn()
-      render(
-        <EditableCollectionTitle
-          collectionId="testCollection123"
-          text="Test Collection"
-          onSave={handleOnSave}
-          onDelete={jest.fn()}
-        />
-      )
-      const editTitle = screen.getByRole('button', {
-        name: 'Edit Test Collection collection title',
-      })
-      userEvent.click(editTitle)
-      const input = screen.getByRole('textbox')
-      userEvent.click(input)
-      userEvent.clear(input)
-      userEvent.type(input, 'Updated Title')
-      userEvent.tab()
-      expect(handleOnSave).toHaveBeenCalled()
-      expect(
-        screen.getByRole('button', {
-          name: 'Edit Updated Title collection title',
-        })
-      ).toHaveTextContent('Updated Title')
-    })
-  })
-
-  it('reverts back to the previous value if the input is cleared', () => {
-    const handleOnSave = jest.fn()
-
-    render(
-      <EditableCollectionTitle
-        collectionId="testCollection123"
-        text="Test Collection"
-        onSave={handleOnSave}
-        onDelete={jest.fn()}
-      />
-    )
-    const editTitle = screen.getByRole('button', {
-      name: 'Edit Test Collection collection title',
-    })
-    userEvent.click(editTitle)
-    const input = screen.getByRole('textbox')
-    userEvent.click(input)
-    userEvent.clear(input)
-    fireEvent.blur(input)
-    expect(handleOnSave).not.toHaveBeenCalled()
-    expect(
-      screen.getByRole('button', {
-        name: 'Edit Test Collection collection title',
-      })
-    ).toHaveTextContent('Test Collection')
-  })
-
-  it('deletes the collection if the input is left empty and there is no existing value', () => {
-    const handleOnSave = jest.fn()
-    const handleOnDelete = jest.fn()
-
-    render(
-      <EditableCollectionTitle
-        collectionId="testCollection123"
-        text=""
-        onSave={handleOnSave}
-        onDelete={handleOnDelete}
-      />
-    )
-
-    const input = screen.getByRole('textbox')
-    userEvent.click(input)
-    fireEvent.blur(input)
-    expect(handleOnDelete).toHaveBeenCalled()
-    expect(handleOnSave).not.toHaveBeenCalled()
-  })
+it('cancels the form', () => {
+  const mockHandleOnCancel = jest.fn()
+  render(
+    <EditableCollectionTitle
+      collectionId="testCollection123"
+      text="Test Collection"
+      onSave={jest.fn()}
+      onCancel={mockHandleOnCancel}
+      onDelete={jest.fn()}
+      isEditing={true}
+    />
+  )
+  const cancelButton = screen.getByRole('button', { name: 'Cancel' })
+  userEvent.click(cancelButton)
+  expect(mockHandleOnCancel).toBeCalledTimes(1)
 })
