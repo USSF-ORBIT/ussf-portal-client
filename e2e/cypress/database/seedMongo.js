f1eb145cd5eb75305f682083efac3bf0e85dba21
Abigail Young <abbyoung@gmail.com>
2022-01-31 11:52:44 -0800

    reverting change to cypress test db name

--- e2e/cypress/database/seedMongo.js
+++ e2e/cypress/database/seedMongo.js
@@ -2,7 +2,7 @@
 const { MongoClient } = require('mongodb')
 const { testUser1, testUser2 } = require('./users.js')
 
-const DB = 'dev'
+const DB = 'cypress-e2e'
 
 async function dropCollection(mongoClient, collectionName) {
   const collection = mongoClient.db(DB).collection(collectionName)
