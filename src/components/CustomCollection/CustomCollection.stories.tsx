a339375be62da9b72ce75e99cab599f472e72b93
Suzanne Rozier <suz@truss.works>
2022-02-01 15:36:00 -0600

    Add tests, stories

--- src/components/CustomCollection/CustomCollection.stories.tsx
+++ src/components/CustomCollection/CustomCollection.stories.tsx
@@ -45,6 +45,135 @@ export const ExampleCustomCollection = (argTypes: StorybookArgTypes) => (
   />
 )
 
+export const WithNineLinks = (argTypes: StorybookArgTypes) => (
+  <CustomCollection
+    _id="testCollection"
+    title="Example collection"
+    bookmarks={[
+      {
+        _id: '1',
+        url: 'https://google.com',
+        label: 'Webmail',
+        cmsId: 'cmsId1',
+      },
+      {
+        _id: '2',
+        url: 'https://mypay.dfas.mil/#/',
+        label: 'MyPay',
+        cmsId: 'cmsId2',
+      },
+      {
+        _id: '3',
+        url: 'https://afpcsecure.us.af.mil/PKI/MainMenu1.aspx',
+        label: 'vMPF',
+        cmsId: 'cmsId3',
+      },
+      {
+        _id: '4',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '5',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '6',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '7',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '8',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '9',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+    ]}
+    handleAddBookmark={argTypes.handleAddBookmark}
+    handleRemoveBookmark={argTypes.handleRemoveBookmark}
+    handleRemoveCollection={argTypes.handleRemoveCollection}
+    handleEditCollection={argTypes.handleEditCollection}
+    handleEditBookmark={argTypes.handleEditBookmark}
+  />
+)
+
+export const WithTenLinks = (argTypes: StorybookArgTypes) => (
+  <CustomCollection
+    _id="testCollection"
+    title="Example collection"
+    bookmarks={[
+      {
+        _id: '1',
+        url: 'https://google.com',
+        label: 'Webmail',
+        cmsId: 'cmsId1',
+      },
+      {
+        _id: '2',
+        url: 'https://mypay.dfas.mil/#/',
+        label: 'MyPay',
+        cmsId: 'cmsId2',
+      },
+      {
+        _id: '3',
+        url: 'https://afpcsecure.us.af.mil/PKI/MainMenu1.aspx',
+        label: 'vMPF',
+        cmsId: 'cmsId3',
+      },
+      {
+        _id: '4',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '5',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '6',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '7',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '8',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '9',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+      {
+        _id: '10',
+        url: 'https://example.com',
+        label: 'My Custom Link',
+      },
+    ]}
+    handleAddBookmark={argTypes.handleAddBookmark}
+    handleRemoveBookmark={argTypes.handleRemoveBookmark}
+    handleRemoveCollection={argTypes.handleRemoveCollection}
+    handleEditCollection={argTypes.handleEditCollection}
+    handleEditBookmark={argTypes.handleEditBookmark}
+  />
+)
+
 export const BlankCustomCollection = (argTypes: StorybookArgTypes) => (
   <CustomCollection
     _id="testCollection"
