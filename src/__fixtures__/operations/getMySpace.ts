8e202c39d4c04a94035fcd8a9481de68358f18e6
Suzanne Rozier <suz@truss.works>
2022-02-18 12:50:45 -0600

    Updating tests, mock data

---
+++ src/__fixtures__/operations/getMySpace.ts
@@ -0,0 +1,97 @@
+import { GET_MY_SPACE } from 'operations/queries/getMySpace'
+
+export const getMySpaceMock = [
+  {
+    request: {
+      query: GET_MY_SPACE,
+    },
+    result: {
+      data: {
+        mySpace: [
+          {
+            _id: '1',
+            title: 'Example Collection',
+            bookmarks: [
+              {
+                _id: '3',
+                url: 'https://google.com',
+                label: 'Webmail',
+                cmsId: null,
+                isRemoved: null,
+              },
+              {
+                _id: '4',
+                url: 'https://mypay.dfas.mil/#/',
+                label: 'MyPay',
+                cmsId: '1',
+                isRemoved: null,
+              },
+              {
+                _id: '5',
+                url: 'https://afpcsecure.us.af.mil/PKI/MainMenu1.aspx',
+                label: 'vMPF',
+                cmsId: '2',
+                isRemoved: null,
+              },
+            ],
+          },
+          {
+            _id: '2',
+            title: 'Maxed Out Collection',
+            bookmarks: Array.from({ length: 10 }, (x, i) => ({
+              _id: `${i}`,
+              label: `Bookmark ${i}`,
+              url: '#',
+              isRemoved: null,
+              cmsId: `${i}`,
+            })),
+          },
+          {
+            __typename: 'NewsWidget',
+            _id: '3',
+            title: 'Recent News',
+            type: 'News',
+          },
+          ...Array.from({ length: 19 }, (x, i) => ({
+            _id: `${i + 4}`,
+            title: `Collection ${i + 4}`,
+            bookmarks: [],
+          })),
+        ],
+      },
+    },
+  },
+]
+
+const mockCollection = {
+  title: 'Example Collection',
+  bookmarks: [
+    {
+      _id: '3',
+      url: 'https://google.com',
+      label: 'Webmail',
+      cmsId: null,
+      isRemoved: null,
+    },
+  ],
+}
+
+const maxCollections = Array.from({ length: 25 }, (x, i) => {
+  return {
+    ...mockCollection,
+    _id: `${i}`,
+  }
+})
+
+export const getMySpaceMaximumCollectionsMock = [
+  {
+    request: {
+      query: GET_MY_SPACE,
+    },
+    result: {
+      data: {
+        mySpace: maxCollections,
+      },
+    },
+  },
+]
