acd8d6cc0ed7e1db511942ffb369b30f985df61f
Abigail Young <abbyoung@gmail.com>
2022-03-08 11:32:44 -0800

    fixing ecs detector, env var for collector endpoint

--- startup/tracing.js
+++ startup/tracing.js
@@ -6,21 +6,27 @@ const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc')
 const { AWSXRayPropagator } = require('@opentelemetry/propagator-aws-xray')
 const { AWSXRayIdGenerator } = require('@opentelemetry/id-generator-aws-xray')
 const { trace } = require('@opentelemetry/api')
+const { AwsInstrumentation } = require('@opentelemetry/instrumentation-aws-sdk')
+// const { registerInstrumentations } = require('@opentelemetry/instrumentation')
 
 const { detectResources } = require('@opentelemetry/resources')
-const { awsEc2Detector } = require('@opentelemetry/resource-detector-aws')
+const { awsEcsDetector } = require('@opentelemetry/resource-detector-aws')
 
 module.exports.initTracing = async (serviceName) => {
   // Add detector for automatically recording metadata in ECS
   const resource = await detectResources({
-    detectors: [awsEc2Detector],
+    detectors: [awsEcsDetector],
   })
 
   // Create a provider using the AWS ID Generator
   const tracerConfig = {
     idGenerator: new AWSXRayIdGenerator(),
     // Instrumentations can go here if needed
-    instrumentations: [],
+    instrumentations: [
+      new AwsInstrumentation({
+        // see the upstream documentation for available configuration
+      }),
+    ],
     // Resources can go here if needed
     resources: { resource },
   }
@@ -30,7 +36,7 @@ module.exports.initTracing = async (serviceName) => {
   // Add OTLP exporter
   const otlpExporter = new OTLPTraceExporter({
     serviceName,
-    url: 'localhost:4317',
+    url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
     // credentials only required if tls setup on Collector instance,
     // credentials: grpc.credentials.createSsl(
     //   fs.readFileSync("./ca.crt"),
