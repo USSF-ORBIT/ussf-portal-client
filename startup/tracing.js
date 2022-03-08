58790b253a2b71d3a33b777cb49c07fbdc8540c2
Abigail Young <abbyoung@gmail.com>
2022-03-08 12:30:23 -0800

    trying diff url and detectors

--- startup/tracing.js
+++ startup/tracing.js
@@ -10,12 +10,15 @@ const { AwsInstrumentation } = require('@opentelemetry/instrumentation-aws-sdk')
 // const { registerInstrumentations } = require('@opentelemetry/instrumentation')
 
 const { detectResources } = require('@opentelemetry/resources')
-const { awsEcsDetector } = require('@opentelemetry/resource-detector-aws')
+const {
+  awsEcsDetector,
+  awsEc2Detector,
+} = require('@opentelemetry/resource-detector-aws')
 
 module.exports.initTracing = async (serviceName) => {
   // Add detector for automatically recording metadata in ECS
   const resource = await detectResources({
-    detectors: [awsEcsDetector],
+    detectors: [awsEcsDetector, awsEc2Detector],
   })
 
   // Create a provider using the AWS ID Generator
@@ -36,7 +39,7 @@ module.exports.initTracing = async (serviceName) => {
   // Add OTLP exporter
   const otlpExporter = new OTLPTraceExporter({
     serviceName,
-    url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+    url: '127.0.0.1:4317',
     // credentials only required if tls setup on Collector instance,
     // credentials: grpc.credentials.createSsl(
     //   fs.readFileSync("./ca.crt"),
