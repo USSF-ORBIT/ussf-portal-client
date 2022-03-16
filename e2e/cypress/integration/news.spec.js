478c7f9f5134b670d6df34819a03a7db862d5a06
Suzanne Rozier <suz@truss.works>
2022-03-16 16:41:54 -0500

    Update e2e tests

--- e2e/cypress/integration/news.spec.js
+++ e2e/cypress/integration/news.spec.js
@@ -2,8 +2,6 @@ import logging from '../plugins/logging'
 
 describe('News and Announcements', () => {
   before(() => {
-    // Reset the database
-    cy.task('db:seed')
     cy.loginTestIDP()
   })
 
