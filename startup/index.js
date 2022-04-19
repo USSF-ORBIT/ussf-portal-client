6564cd8f58abed7c4cb816c835199d6aa214ac2f
Suzanne Rozier <suz@truss.works>
2022-04-19 15:50:46 -0500

    Check for env vars on startup

--- startup/index.js
+++ startup/index.js
@@ -4,8 +4,32 @@
 const { initLogger } = require('./logging')
 const { runMigrations } = require('./migrate')
 const { initTracing } = require('./tracing')
+require('dotenv').config()
+
+const requireVars = [
+  'MONGO_URL',
+  'MONGODB_DB',
+  'REDIS_URL',
+  'SAML_ISSUER',
+  'SAML_IDP_METADATA_URL',
+  'SAML_SSO_CALLBACK_URL',
+  'SESSION_SECRET',
+  'SESSION_DOMAIN',
+  'MATOMO_URL',
+  'MATOMO_SITE_ID',
+]
+
 function startup() {
   initLogger()
+
+  requireVars.forEach((v) => {
+    if (process.env[`${v}`] === undefined) {
+      throw new Error(
+        `Startup Error: required environment variable ${v} is undefined`
+      )
+    }
+  })
+
   runMigrations()
   initTracing('AWSObservability')
 }
