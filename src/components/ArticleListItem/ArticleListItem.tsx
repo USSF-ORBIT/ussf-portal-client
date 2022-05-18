9d4a8cdc53304006c1f8d8668d5d112b4673dd73
Suzanne Rozier <suz@truss.works>
2022-05-18 11:07:50 -0400

    Styling article date block

--- src/components/ArticleListItem/ArticleListItem.tsx
+++ src/components/ArticleListItem/ArticleListItem.tsx
@@ -4,26 +4,44 @@ import styles from './ArticleListItem.module.scss'
 
 import type { ArticleListItemRecord } from 'types'
 
+const ArticleDateBlock = ({ date }: { date: Date }) => {
+  const dateFormatter = new Intl.DateTimeFormat('en-us', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+  })
+
+  const dateParts = dateFormatter.formatToParts(date)
+  const { value: month } = dateParts.find((i) => i.type === 'month') || {}
+  const { value: day } = dateParts.find((i) => i.type === 'day') || {}
+  const { value: year } = dateParts.find((i) => i.type === 'year') || {}
+
+  if (month === undefined || day === undefined || year === undefined) {
+    // TODO throw error
+  }
+
+  return (
+    <time dateTime={date.toLocaleString()} className={styles.ArticleDateBlock}>
+      <small>{month}</small>
+      <span>{day}</span>
+      <small>{year}</small>
+    </time>
+  )
+}
+
 export const ArticleListItem = ({
   article,
 }: {
   article: ArticleListItemRecord
 }) => {
-  // TODO - style & format date
+  // TODO - add permalink to single article page
   const { /* id, slug,*/ title, preview, publishedDate } = article
 
   const publishedDateObj = new Date(publishedDate)
 
   return (
     <article className={styles.ArticleListItem}>
-      <time dateTime={publishedDate}>
-        {publishedDateObj.toLocaleDateString([], {
-          year: 'numeric',
-          month: 'short',
-          day: '2-digit',
-        })}
-      </time>
-
+      <ArticleDateBlock date={publishedDateObj} />
       <div>
         <h4>{title}</h4>
         <p>{preview}</p>
