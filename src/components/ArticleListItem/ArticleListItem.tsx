da5d6631993a4e6a06e09b2fc6c5c7f32ffe2b92
Suzanne Rozier <suz@truss.works>
2022-05-19 10:15:55 -0400

    Split out date icon component, add tests

--- src/components/ArticleListItem/ArticleListItem.tsx
+++ src/components/ArticleListItem/ArticleListItem.tsx
@@ -3,32 +3,8 @@ import React from 'react'
 import styles from './ArticleListItem.module.scss'
 
 import type { ArticleListItemRecord } from 'types'
+import { ArticleDateIcon } from 'components/ArticleDateIcon/ArticleDateIcon'
 import LinkTo from 'components/util/LinkTo/LinkTo'
-
-const ArticleDateBlock = ({ date }: { date: Date }) => {
-  const dateFormatter = new Intl.DateTimeFormat('en-us', {
-    year: 'numeric',
-    month: 'short',
-    day: '2-digit',
-  })
-
-  const dateParts = dateFormatter.formatToParts(date)
-  const { value: month } = dateParts.find((i) => i.type === 'month') || {}
-  const { value: day } = dateParts.find((i) => i.type === 'day') || {}
-  const { value: year } = dateParts.find((i) => i.type === 'year') || {}
-
-  if (month === undefined || day === undefined || year === undefined) {
-    // TODO throw error
-  }
-
-  return (
-    <time dateTime={date.toLocaleString()} className={styles.ArticleDateBlock}>
-      <small>{month}</small>
-      <span>{day}</span>
-      <small>{year}</small>
-    </time>
-  )
-}
 
 export const ArticleListItem = ({
   article,
@@ -41,12 +17,12 @@ export const ArticleListItem = ({
 
   return (
     <article className={styles.ArticleListItem}>
-      <ArticleDateBlock date={publishedDateObj} />
+      <ArticleDateIcon date={publishedDateObj} />
       <div>
-        <h4>{title}</h4>
+        <h4>
+          <LinkTo href={`/articles/${slug}`}>{title}</LinkTo>
+        </h4>
         <p>{preview}</p>
-        <br />
-        <LinkTo href={`/articles/${slug}`}>Read more...</LinkTo>
       </div>
     </article>
   )
