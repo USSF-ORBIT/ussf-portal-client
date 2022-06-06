9a15ecd176e040a8525b3fc5bc3b2c9707080205
Suzanne Rozier <suz@truss.works>
2022-06-06 13:29:56 -0500

    feat: Portal product development blog (#642)
    * Portal news page, article list item component
    
    * Create and style ArticleList component
    
    * Styling article date block
    
    * Add single article page and query
    
    * Split out date icon component, add tests
    
    * Split out date icon component, add tests
    
    * Add tests for portal news page
    
    * Render article in component
    
    * Use article category, update routes
    
    * Split out ArticleLayout, different nav for different categories
    
    * Add empty state to article list and tests
    
    * Clean up, add article page tests
    
    * 404 if article is unpublished
    
    * Add tests for article serverside props
    
    * Query article status on single page

---
+++ src/pages/articles/[article].tsx
@@ -0,0 +1,111 @@
+import { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+
+import {
+  BreadcrumbBar,
+  Breadcrumb,
+  BreadcrumbLink,
+  GridContainer,
+} from '@trussworks/react-uswds'
+
+import { client } from 'lib/keystoneClient'
+import { useUser } from 'hooks/useUser'
+import Loader from 'components/Loader/Loader'
+import { withArticleLayout } from 'layout/DefaultLayout/ArticleLayout'
+import NavLink, { NavLinkProps } from 'components/util/NavLink/NavLink'
+import PageHeader from 'components/PageHeader/PageHeader'
+import { GET_ARTICLE } from 'operations/cms/queries/getArticle'
+import { SingleArticle } from 'components/SingleArticle/SingleArticle'
+
+const ORBITBlogArticleHeader = () => (
+  <PageHeader disableSearch>
+    <div>
+      <h1>ORBIT Blog</h1>
+      <BreadcrumbBar>
+        <Breadcrumb>
+          <BreadcrumbLink<NavLinkProps> asCustom={NavLink} href="/">
+            Service portal home
+          </BreadcrumbLink>
+        </Breadcrumb>
+        <Breadcrumb>
+          <BreadcrumbLink<NavLinkProps> asCustom={NavLink} href="/about-us">
+            About us
+          </BreadcrumbLink>
+        </Breadcrumb>
+        <Breadcrumb current>
+          <BreadcrumbLink<NavLinkProps>
+            asCustom={NavLink}
+            href="/about-us/orbit-blog">
+            ORBIT Blog
+          </BreadcrumbLink>
+        </Breadcrumb>
+      </BreadcrumbBar>
+    </div>
+  </PageHeader>
+)
+
+const InternalNewsArticleHeader = () => (
+  <PageHeader disableSearch>
+    <div>
+      <h1>News</h1>
+      <BreadcrumbBar>
+        <Breadcrumb>
+          <BreadcrumbLink<NavLinkProps> asCustom={NavLink} href="/">
+            Service portal home
+          </BreadcrumbLink>
+        </Breadcrumb>
+        <Breadcrumb current>
+          <BreadcrumbLink<NavLinkProps> asCustom={NavLink} href="/news">
+            News
+          </BreadcrumbLink>
+        </Breadcrumb>
+      </BreadcrumbBar>
+    </div>
+  </PageHeader>
+)
+
+const SingleArticlePage = ({
+  article,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { user } = useUser()
+  const { category } = article
+
+  return (
+    <>
+      {category === 'ORBITBlog' ? (
+        <ORBITBlogArticleHeader />
+      ) : (
+        <InternalNewsArticleHeader />
+      )}
+      <GridContainer>
+        {!user ? <Loader /> : <SingleArticle article={article} />}
+      </GridContainer>
+    </>
+  )
+}
+
+export default SingleArticlePage
+
+SingleArticlePage.getLayout = (page: React.ReactNode) => withArticleLayout(page)
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { article: slug } = context.query
+
+  const {
+    data: { article },
+  } = await client.query({
+    query: GET_ARTICLE,
+    variables: { slug },
+  })
+
+  if (!article || article.status !== 'Published') {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      article,
+    },
+  }
+}
