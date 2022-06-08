5ea4b1d48ab0ce922026e18a1c02706ba4ea19ba
Suzanne Rozier <suz@truss.works>
2022-06-08 16:34:19 -0500

    Add search operation and page

---
+++ src/operations/cms/queries/search.ts
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client'
+
+export const SEARCH = gql`
+  query Search($query: String!) {
+    search(query: $query) {
+      type
+      title
+      url
+      description
+    }
+  }
+`
---
+++ src/pages/search.tsx
@@ -0,0 +1,70 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import {
+  BreadcrumbBar,
+  Breadcrumb,
+  BreadcrumbLink,
+  GridContainer,
+} from '@trussworks/react-uswds'
+
+import { client } from 'lib/keystoneClient'
+import Loader from 'components/Loader/Loader'
+import { useUser } from 'hooks/useUser'
+import { withArticleLayout } from 'layout/DefaultLayout/ArticleLayout'
+import NavLink, { NavLinkProps } from 'components/util/NavLink/NavLink'
+import PageHeader from 'components/PageHeader/PageHeader'
+import { SEARCH } from 'operations/cms/queries/search'
+// import styles from 'styles/pages/news.module.scss'
+
+const Search = ({
+  query,
+  results,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { user } = useUser()
+
+  return (
+    <>
+      <PageHeader disableSearch>
+        <div>
+          <h1>Search</h1>
+          <BreadcrumbBar>
+            <Breadcrumb>
+              <BreadcrumbLink<NavLinkProps> asCustom={NavLink} href="/">
+                Service portal home
+              </BreadcrumbLink>
+            </Breadcrumb>
+            <Breadcrumb current>
+              <BreadcrumbLink<NavLinkProps> asCustom={NavLink} href="/search">
+                Search
+              </BreadcrumbLink>
+            </Breadcrumb>
+          </BreadcrumbBar>
+        </div>
+      </PageHeader>
+      <GridContainer>
+        {!user ? <Loader /> : `Search for ${query}`}
+      </GridContainer>
+    </>
+  )
+}
+
+export default Search
+
+Search.getLayout = (page: React.ReactNode) => withArticleLayout(page)
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // get search terms from URL params
+  const { q } = context.query
+
+  const { data } = await client.query({
+    query: SEARCH,
+    variables: { query: `${q} milmove` },
+  })
+
+  console.log('returned', data)
+
+  return {
+    props: {
+      query: q,
+    },
+  }
+}
