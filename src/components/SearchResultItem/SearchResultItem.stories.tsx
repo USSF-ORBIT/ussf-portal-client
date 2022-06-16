90c92f3932db43b2a95f9fde498bf85f57df61e5
Suzanne Rozier <suz@truss.works>
2022-06-16 15:57:29 -0500

    dev: Search UI components (#661)
    * SearchBanner component
    
    * Style various uses of USWDSTag
    
    * SearchResultsItem component
    
    * Style tweaks
    
    * Update SearchResultItem design
    
    * Style with line clamping
    
    * Add tests for new components and comments
    
    * Design tweaks, new copy for search banners

---
+++ src/components/SearchResultItem/SearchResultItem.stories.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+
+import { SearchResultItem, SearchResultRecord } from './SearchResultItem'
+
+export default {
+  title: 'Components/SearchResultItem',
+  component: SearchResultItem,
+  decorators: [
+    (Story) => (
+      <div className="sfds">
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta
+
+const testApplicationResult: SearchResultRecord = {
+  id: 'testBookmark123',
+  type: 'Bookmark',
+  title: 'MyFSS',
+  preview:
+    'How is Air Force Information Management System abbreviated? AFIMS stands for Air Force Information Management System. For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective.',
+  permalink: 'https://www.url.com/myfss',
+}
+
+export const ApplicationResult = () => (
+  <SearchResultItem item={testApplicationResult} />
+)
+
+const testArticleResult: SearchResultRecord = {
+  id: 'testArticle123',
+  type: 'Article',
+  title:
+    'Physical training: Everything you need to know about the update in requirements',
+  preview:
+    'As a steward of almost 9 million acres encompassing forests, prairies, deserts, wetlands, and coastal habitats, the Department of the Air Force recognizes the importance of protecting and sustaining the natural environment.',
+  permalink:
+    'https://localhost:3000/articles/physical-training-everything-you-need-to-know',
+  date: '2022-05-17T13:44:39.796Z',
+  labels: [
+    {
+      id: 'testLabel',
+      name: 'All Guardians',
+      type: 'Audience',
+    },
+  ],
+}
+
+export const ArticleResult = () => <SearchResultItem item={testArticleResult} />
+
+export const ArticleResultLineClamp = () => (
+  <SearchResultItem
+    item={{
+      ...testArticleResult,
+      preview:
+        'As a steward of almost 9 million acres encompassing forests, prairies, deserts, wetlands, and coastal habitats, the Department of the Air Force recognizes the importance of protecting and sustaining the natural environment. As a steward of almost 9 million acres encompassing forests, prairies, deserts, wetlands, and coastal habitats, the Department of the Air Force recognizes the importance of protecting and sustaining the natural environment. As a steward of almost 9 million acres encompassing forests, prairies, deserts, wetlands, and coastal habitats, the Department of the Air Force recognizes the importance of protecting and sustaining the natural environment.',
+      permalink:
+        'https://localhost:3000/articles/physical-training-everything-you-need-to-know?asfljaslfkjlkajrlakjsflkajsflasfmaaslkfjalsfjaklsjlasfksfljaslfkjlkajrlakjsflkajsflasfmaaslkfjalsfjaklsjlasfk',
+    }}
+  />
+)
