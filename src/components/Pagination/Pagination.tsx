6ab986b66c9bd8bc09517318c0ba79298634d889
Suzanne Rozier <suz@truss.works>
2022-06-28 16:47:42 -0500

    Clean up and add comments

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -71,68 +71,64 @@ const Pagination = ({
   const showNextOverflow =
     showOverflow && pages.length - currentPage >= middleSlot
 
-  const prevSlots = isOnFirstPage ? 0 : showPrevOverflow ? 2 : 1 // first page, prev overflow
-  const nextSlots = isOnLastPage ? 0 : showNextOverflow ? 2 : 1 // next overflow, last page
-  const pageRangeSize = maxSlots - 1 - (prevSlots + nextSlots) // minus one for the current page
+  // Assemble array of page numbers to be shown
+  const currentPageRange: Array<number | 'overflow'> = showOverflow
+    ? [currentPage]
+    : Array.from({ length: pages.length }).map((_, i) => i + 1)
 
-  // Populate the remaining slots
-  let currentPageBeforeSize = 0
-  let currentPageAfterSize = 0
-  if (showPrevOverflow && showNextOverflow) {
-    currentPageBeforeSize = Math.round((pageRangeSize - 1) / 2)
-    currentPageAfterSize = pageRangeSize - currentPageBeforeSize
-  } else if (showPrevOverflow) {
-    currentPageAfterSize = pages.length - currentPage - 1 // current & last
-    currentPageAfterSize = currentPageAfterSize < 0 ? 0 : currentPageAfterSize
-    currentPageBeforeSize = pageRangeSize - currentPageAfterSize
-  } else if (showNextOverflow) {
-    currentPageBeforeSize = currentPage - 2 // first & current
-    currentPageBeforeSize =
-      currentPageBeforeSize < 0 ? 0 : currentPageBeforeSize
-    currentPageAfterSize = pageRangeSize - currentPageBeforeSize
+  if (showOverflow) {
+    // Determine range of pages to show based on current page & number of slots
+    // Follows logic described at: https://designsystem.digital.gov/components/pagination/
+    const prevSlots = isOnFirstPage ? 0 : showPrevOverflow ? 2 : 1 // first page + prev overflow
+    const nextSlots = isOnLastPage ? 0 : showNextOverflow ? 2 : 1 // next overflow + last page
+    const pageRangeSize = maxSlots - 1 - (prevSlots + nextSlots) // remaining slots to show (minus one for the current page)
+
+    // Determine how many slots we have before/after the current page
+    let currentPageBeforeSize = 0
+    let currentPageAfterSize = 0
+    if (showPrevOverflow && showNextOverflow) {
+      // We are in the middle of the set, there will be overflow (...) at both the beginning & end
+      // Ex: [1] [...] [9] [10] [11] [...] [24]
+      currentPageBeforeSize = Math.round((pageRangeSize - 1) / 2)
+      currentPageAfterSize = pageRangeSize - currentPageBeforeSize
+    } else if (showPrevOverflow) {
+      // We are in the end of the set, there will be overflow (...) at the beginning
+      // Ex: [1] [...] [20] [21] [22] [23] [24]
+      currentPageAfterSize = pages.length - currentPage - 1 // current & last
+      currentPageAfterSize = currentPageAfterSize < 0 ? 0 : currentPageAfterSize
+      currentPageBeforeSize = pageRangeSize - currentPageAfterSize
+    } else if (showNextOverflow) {
+      // We are in the beginning of the set, there will be overflow (...) at the end
+      // Ex: [1] [2] [3] [4] [5] [...] [24]
+      currentPageBeforeSize = currentPage - 2 // first & current
+      currentPageBeforeSize =
+        currentPageBeforeSize < 0 ? 0 : currentPageBeforeSize
+      currentPageAfterSize = pageRangeSize - currentPageBeforeSize
+    }
+
+    // Populate the remaining slots
+    let counter = 1
+    while (currentPageBeforeSize > 0) {
+      // Add previous pages before the current page
+      currentPageRange.unshift(currentPage - counter)
+      counter++
+      currentPageBeforeSize--
+    }
+
+    counter = 1
+    while (currentPageAfterSize > 0) {
+      // Add subsequent pages after the current page
+      currentPageRange.push(currentPage + counter)
+      counter++
+      currentPageAfterSize--
+    }
+
+    // Add prev/next overflow indicators, and first/last pages as needed
+    if (showPrevOverflow) currentPageRange.unshift('overflow')
+    if (currentPage !== 1) currentPageRange.unshift(1)
+    if (showNextOverflow) currentPageRange.push('overflow')
+    if (currentPage !== pages.length) currentPageRange.push(pages.length)
   }
-
-  const currentPageRange: Array<number | 'overflow'> = [currentPage] // Start from the current page
-  let counter = 1
-  while (currentPageBeforeSize > 0) {
-    currentPageRange.unshift(currentPage - counter)
-    counter++
-    currentPageBeforeSize--
-  }
-
-  counter = 1
-  while (currentPageAfterSize > 0) {
-    currentPageRange.push(currentPage + counter)
-    counter++
-    currentPageAfterSize--
-  }
-
-  if (showPrevOverflow) currentPageRange.unshift('overflow')
-  if (currentPage !== 1) currentPageRange.unshift(1)
-  if (showNextOverflow) currentPageRange.push('overflow')
-  if (currentPage !== pages.length) currentPageRange.push(pages.length)
-
-  const pageItems = !showOverflow
-    ? pages.map((p, i) => (
-        <PaginationPage
-          key={`pagination_page_${i + 1}`}
-          page={p}
-          pageNum={i + 1}
-          isCurrent={i + 1 === currentPage}
-        />
-      ))
-    : currentPageRange.map((pageNum, i) =>
-        pageNum === 'overflow' ? (
-          <PaginationOverflow key={`pagination_overflow_${i}`} />
-        ) : (
-          <PaginationPage
-            key={`pagination_page_${pageNum}`}
-            pageNum={pageNum}
-            page={pages[pageNum - 1]}
-            isCurrent={pageNum === currentPage}
-          />
-        )
-      )
 
   const prevPage = !isOnFirstPage && pages[currentPage - 2]
   const nextPage = !isOnLastPage && pages[currentPage]
@@ -152,7 +148,18 @@ const Pagination = ({
           </li>
         )}
 
-        {pageItems}
+        {currentPageRange.map((pageNum, i) =>
+          pageNum === 'overflow' ? (
+            <PaginationOverflow key={`pagination_overflow_${i}`} />
+          ) : (
+            <PaginationPage
+              key={`pagination_page_${pageNum}`}
+              pageNum={pageNum}
+              page={pages[pageNum - 1]}
+              isCurrent={pageNum === currentPage}
+            />
+          )
+        )}
 
         {nextPage && (
           <li className="usa-pagination__item usa-pagination__arrow">
