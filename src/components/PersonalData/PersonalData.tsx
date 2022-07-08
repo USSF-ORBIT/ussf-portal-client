f7078763b20aba9cdbe92bb3632788f1e5e5c9f8
Jacob Capps <jcapps@truss.works>
2022-07-08 15:56:44 -0500

    Add Edit name to homepage

--- src/components/PersonalData/PersonalData.tsx
+++ src/components/PersonalData/PersonalData.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
-
+import { useRouter } from 'next/router'
+import { Breadcrumb, BreadcrumbLink } from '@trussworks/react-uswds'
 import styles from './PersonalData.module.scss'
-
+import NavLink, { NavLinkProps } from 'components/util/NavLink/NavLink'
 import { useAuthContext } from 'stores/authContext'
 
 const PersonalData = () => {
   const { user } = useAuthContext()
+  const router = useRouter()
 
   const greeting = user
     ? `Welcome, ${user.attributes.givenname} ${user.attributes.surname}`
     : 'Welcome!'
 
+  const currentPage = router.pathname
+
   return (
     <div className={styles.personalData}>
       <h2>{greeting}</h2>
+
+      {currentPage != '/settings' && (
+        <div>
+          <Breadcrumb>
+            <BreadcrumbLink<NavLinkProps> asCustom={NavLink} href="/settings">
+              Edit name
+            </BreadcrumbLink>
+          </Breadcrumb>
+        </div>
+      )}
     </div>
   )
 }
