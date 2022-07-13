32f55159e6eef44b42e0589a6516cb67d5bfc582
Jacob Capps <jcapps@truss.works>
2022-07-13 16:46:25 -0500

    Remove unused line

--- src/layout/DefaultLayout/DefaultLayout.test.tsx
+++ src/layout/DefaultLayout/DefaultLayout.test.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
 import { getDisplayNameMock } from '../../__fixtures__/operations/getDisplayName'
-import DefaultLayout, { withDefaultLayout } from './DefaultLayout'
+import DefaultLayout from './DefaultLayout'
 
 jest.mock('next/router', () => ({
   useRouter: jest.fn().mockReturnValue({
