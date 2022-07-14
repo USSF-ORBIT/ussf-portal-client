44a4ab663a6328f8e524f5df16fc7f311c4b6539
Jacob Capps <jcapps@truss.works>
2022-07-14 15:56:13 -0500

    Update test

--- src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
+++ src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 
 import AnnouncementInfo from './AnnouncementInfo'
 
-const testAnnouncement = {
+const testAnnouncementWithUrl = {
   id: 'testAnnouncement123',
   title: 'Test Announcement',
   body: {
@@ -16,7 +16,89 @@ const testAnnouncement = {
         type: 'paragraph',
         children: [
           {
-            text: 'this is a test',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+        ],
+      },
+      {
+        type: 'component-block',
+        props: {
+          link: {
+            value: 'https://www.google.com',
+            discriminant: 'url',
+          },
+          ctaText: 'View more',
+        },
+        children: [
+          {
+            type: 'component-inline-prop',
+            children: [
+              {
+                text: '',
+              },
+            ],
+          },
+        ],
+        component: 'callToAction',
+      },
+      {
+        type: 'paragraph',
+        children: [
+          {
+            text: '',
+          },
+        ],
+      },
+    ],
+  },
+  status: 'Published',
+  publishedDate: '2022-05-17T13:44:39.796Z',
+}
+
+const testAnnouncementWithArticle = {
+  id: 'anotherTest123',
+  title: 'Cool new article',
+  body: {
+    document: [
+      {
+        type: 'paragraph',
+        children: [
+          {
+            text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat mas',
+          },
+        ],
+      },
+      {
+        type: 'component-block',
+        props: {
+          link: {
+            value: {
+              id: 'cl4ydwrah1780zhrimycrfati',
+              data: {
+                slug: '/something',
+              },
+            },
+            discriminant: 'article',
+          },
+          ctaText: 'View article',
+        },
+        children: [
+          {
+            type: 'component-inline-prop',
+            children: [
+              {
+                text: '',
+              },
+            ],
+          },
+        ],
+        component: 'callToAction',
+      },
+      {
+        type: 'paragraph',
+        children: [
+          {
+            text: '',
           },
         ],
       },
@@ -27,9 +109,15 @@ const testAnnouncement = {
 }
 
 describe('AnnouncementInfo component', () => {
-  it('renders the announcement info', () => {
-    render(<AnnouncementInfo announcement={testAnnouncement} />)
+  it('renders an announcement with a url CTA', () => {
+    render(<AnnouncementInfo announcement={testAnnouncementWithUrl} />)
 
     expect(screen.getAllByText('Test Announcement')).toHaveLength(1)
+  })
+
+  it('renders an announcement with an article CTA', () => {
+    render(<AnnouncementInfo announcement={testAnnouncementWithArticle} />)
+
+    expect(screen.getAllByText('Cool new article')).toHaveLength(1)
   })
 })
