da4204bdf40a95bd2395351eceb86f2c44c6781b
Jacob Capps <jcapps@truss.works>
2022-07-21 11:17:10 -0500

    Remove unnecessary code

--- src/components/NewsListItem/NewsListItem.tsx
+++ src/components/NewsListItem/NewsListItem.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import classnames from 'classnames'
-import { Grid, Tag } from '@trussworks/react-uswds'
+import { Tag } from '@trussworks/react-uswds'
 import styles from './NewsListItem.module.scss'
 import colors from 'styles/sfds/colors.module.scss'
 
-import { Category, Label } from 'components/Tag/Tag'
 import LinkTo from 'components/util/LinkTo/LinkTo'
 import type { NewsListItemArticle } from 'types'
-import { CONTENT_CATEGORIES } from 'constants/index'
 
 const NewsListItem = ({
   article,
@@ -18,77 +16,32 @@ const NewsListItem = ({
 }) => {
   const { title, sourceLink, description, publishDate, sourceName } = article
 
-  const x = publishDate.split(' ')
-  const MONTH = x[0]
-  const DAY = x[1].replace(',', '')
-
   return (
-    <>
-      {!widget ? (
-        <article>
-          <Grid row gap={4}>
-            <Grid col="auto">
-              <div className={styles.articlePublishedDate}>
-                <span className={styles.month}>{MONTH}</span>
-                <span className={styles.day}>{DAY}</span>
-              </div>
-            </Grid>
+    <article
+      className={classnames(styles.NewsListItem, {
+        [styles.newsWidgetItem]: widget,
+      })}>
+      <LinkTo
+        href={sourceLink}
+        className={classnames(styles.articleLink, 'usa-link--external')}
+        target="_blank"
+        rel="noreferrer noopener">
+        {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
+        <small>{publishDate} //</small>
+        &nbsp;
+        <h4>{title}</h4>
+      </LinkTo>
 
-            <Grid col="fill" gap="05">
-              <h3 className={styles.articleTitle}>
-                <LinkTo
-                  href={sourceLink}
-                  target="_blank"
-                  rel="noreferrer noopener">
-                  {title}
-                </LinkTo>
-              </h3>
-              <LinkTo
-                href={sourceLink}
-                target="_blank"
-                rel="noreferrer noopener">
-                {sourceLink}
-              </LinkTo>
-              <p>
-                <span>{description}</span>
-              </p>
-              <Grid row gap={4} className={styles.categoryAndLabel}>
-                <Category category={CONTENT_CATEGORIES.EXTERNAL_NEWS} />
-                <Label type="Source">{sourceName}</Label>
-              </Grid>
-            </Grid>
-          </Grid>
-        </article>
-      ) : (
-        <article
-          className={classnames(styles.NewsListItem, {
-            [styles.newsWidgetItem]: widget,
-          })}>
-          <LinkTo
-            href={sourceLink}
-            className={classnames(styles.articleLink, 'usa-link--external')}
-            target="_blank"
-            rel="noreferrer noopener">
-            {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
-            <small>{publishDate} //</small>
-            &nbsp;
-            <h4>{title}</h4>
-          </LinkTo>
+      <p className={styles.articleExcerpt}>
+        <span className={styles.articleExcerptTruncate}>
+          {description}&hellip;
+        </span>
+      </p>
 
-          <p className={styles.articleExcerpt}>
-            <span className={styles.articleExcerptTruncate}>
-              {description}&hellip;
-            </span>
-          </p>
-
-          <Tag
-            className={styles.articleTag}
-            background={colors['theme-mars-base']}>
-            {sourceName}
-          </Tag>
-        </article>
-      )}
-    </>
+      <Tag className={styles.articleTag} background={colors['theme-mars-base']}>
+        {sourceName}
+      </Tag>
+    </article>
   )
 }
 
