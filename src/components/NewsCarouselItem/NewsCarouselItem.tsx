03cdf502d8efd2203dbcb8268eaef3f8c852b2fb
Jacob Capps <jcapps@truss.works>
2022-08-04 13:35:18 -0500

    Update storybook component

--- src/components/NewsCarouselItem/NewsCarouselItem.tsx
+++ src/components/NewsCarouselItem/NewsCarouselItem.tsx
@@ -1,19 +1,22 @@
 import { Grid } from '@trussworks/react-uswds'
 import React from 'react'
+import styles from './NewsCarouselItem.module.scss'
 import { ArticleListItemRecord } from 'types'
 
 const NewsCarouselItem = ({ article }: { article: ArticleListItemRecord }) => {
   return (
-    <Grid row>
-      <Grid>
+    <Grid row col="auto">
+      <Grid col={5}>
         <img
           src="https://media.defense.gov/2021/Aug/23/2002905775/670/394/0/210823-F-GO452-0001.JPG"
           alt="test"
         />
       </Grid>
-      <Grid>
-        <h3>{article.title}</h3>
-        <p>{article.preview}</p>
+      <Grid col={7}>
+        <div className={styles.textContainer}>
+          <h1>{article.title}</h1>
+          <div>{article.preview}</div>
+        </div>
       </Grid>
     </Grid>
   )
