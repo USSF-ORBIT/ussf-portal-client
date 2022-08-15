67fd32bc38713e49dec1de11a5834826a7ef044a
Jacob Capps <jcapps@truss.works>
2022-08-15 16:55:09 -0500

    Update styles

--- src/components/NewsCarouselItem/NewsCarouselItem.tsx
+++ src/components/NewsCarouselItem/NewsCarouselItem.tsx
@@ -6,15 +6,22 @@ import LinkTo from 'components/util/LinkTo/LinkTo'
 
 const NewsCarouselItem = ({ article }: { article: ArticleListItemRecord }) => {
   return (
-    <Grid row col="auto">
-      <Grid col="auto">
+    <Grid row>
+      <Grid
+        tablet={{ col: 'fill' }}
+        desktop={{ col: 'auto' }}
+        className={styles.imageContainer}>
         {/* TODO: current image is a placeholder and will need to be updated */}
         <img
           src="https://media.defense.gov/2021/Aug/23/2002905775/670/394/0/210823-F-GO452-0001.JPG"
           alt="test"
+          className={styles.carouselImage}
         />
       </Grid>
-      <Grid col="fill" className={styles.gridContainer}>
+      <Grid
+        tablet={{ col: 'auto' }}
+        desktop={{ col: 'fill' }}
+        className={styles.gridContainer}>
         <Grid className={styles.textContainer}>
           <h1 className={styles.articleTitle}>{article.title}</h1>
           <div className={styles.articlePreview}>{article.preview}</div>
