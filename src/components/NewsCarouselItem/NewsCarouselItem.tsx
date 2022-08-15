9b1914c3795df302bbd3e726756bb72bce091d41
Jacob Capps <jcapps@truss.works>
2022-08-15 13:46:56 -0500

    Add comment

--- src/components/NewsCarouselItem/NewsCarouselItem.tsx
+++ src/components/NewsCarouselItem/NewsCarouselItem.tsx
@@ -8,6 +8,7 @@ const NewsCarouselItem = ({ article }: { article: ArticleListItemRecord }) => {
   return (
     <Grid row col="auto">
       <Grid col="auto">
+        {/* TODO: current image is a placeholder and will need to be updated */}
         <img
           src="https://media.defense.gov/2021/Aug/23/2002905775/670/394/0/210823-F-GO452-0001.JPG"
           alt="test"
