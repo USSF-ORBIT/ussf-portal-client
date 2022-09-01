f171ed4d1c2bc7ec4f8b10ec7db390c18f3cc4a5
Jacob Capps <jcapps@truss.works>
2022-09-01 10:22:09 -0500

    Add ThemeToggle component

---
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './ThemeToggle.module.scss'
+
+const ThemeToggle = () => {
+  return (
+    <div className={styles.toggleContainer}>
+      <p>LIGHT MODE</p>
+    </div>
+  )
+}
+
+export default ThemeToggle
