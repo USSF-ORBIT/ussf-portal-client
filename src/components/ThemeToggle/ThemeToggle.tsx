f53066b3c90732bcea579c2be056796d059fde25
Jacob Capps <jcapps@truss.works>
2022-09-01 12:02:17 -0500

    Update styles and add mock logic

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './ThemeToggle.module.scss'
 
 const ThemeToggle = () => {
+  // TODO: Replace this with the actual logic for changing the theme
+  const [theme, setTheme] = useState('light')
+
+  const handleTheme = () => {
+    return theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+
   return (
-    <div className={styles.toggleContainer}>
-      <p>LIGHT MODE</p>
-    </div>
+    <button type="button" onClick={handleTheme} className={styles.toggleButton}>
+      {theme}
+    </button>
   )
 }
 
