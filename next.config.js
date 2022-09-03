e74d420a4e30327623a0987b3577688b1abda557
Abigail Young <abbyoung@gmail.com>
2022-09-02 17:02:52 -0700

    remove path, pin version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
-/* eslint-disable @typescript-eslint/no-var-requires */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
