868cc77aef63c60079a15e321d3903c9152be905
Jacob Capps <jcapps@truss.works>
2022-09-07 16:33:23 -0500

    Add tracking for toggle

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import styles from './ThemeToggle.module.scss'
+import { useAnalytics } from 'stores/analyticsContext'
 
 const ThemeToggle = () => {
   // TODO: Replace this with the actual logic for changing the theme
   const [theme, setTheme] = useState('light')
+  const { trackEvent } = useAnalytics()
 
   const handleTheme = () => {
+    trackEvent(
+      'Dark mode',
+      'Click on light/dark mode toggle',
+      'Light/Dark mode toggle'
+    )
+
     if (theme === 'light') {
       localStorage.setItem('theme', 'dark')
       setTheme('dark')
