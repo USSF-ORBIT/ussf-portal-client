768c7a19906a8888c746a677180e8591b71e1e52
Jacob Capps <jcapps@truss.works>
2022-09-08 10:10:55 -0500

    Add test for calling trackEvent

--- src/components/ThemeToggle/ThemeToggle.test.tsx
+++ src/components/ThemeToggle/ThemeToggle.test.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import ThemeToggle from './ThemeToggle'
+import * as analyticsHooks from 'stores/analyticsContext'
 
 describe('ThemeToggle component', () => {
   it('renders the component', () => {
@@ -18,4 +19,27 @@ describe('ThemeToggle component', () => {
   })
 })
 
-// TODO: Once we are displaying the ThemeToggle component, add test to check that trackEvent is called
+describe('calling trackEvent function', () => {
+  const mockTrackEvents = jest.fn()
+
+  beforeEach(() => {
+    // mock out the return of useAnalytics since we just need to check that it's called not what it does
+    jest.spyOn(analyticsHooks, 'useAnalytics').mockImplementation(() => {
+      return { push: jest.fn(), trackEvent: mockTrackEvents }
+    })
+
+    render(<ThemeToggle />)
+  })
+
+  it('calls trackEvent when the toggle is clicked', () => {
+    const toggle = screen.getByTestId('theme-toggle')
+    userEvent.click(toggle)
+
+    expect(mockTrackEvents).toHaveBeenCalledTimes(1)
+    expect(mockTrackEvents).toHaveBeenCalledWith(
+      'Dark mode',
+      'Click on light/dark mode toggle',
+      'Light/Dark mode toggle'
+    )
+  })
+})
