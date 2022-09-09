92a1e725f03c892866c8a0dd8941a2414c86b679
John Gedeon <john@truss.works>
2022-09-08 17:08:47 -0700

    ci: Setup happo testing in edge, chrome, and firefox desktop size (#768)
    * build: add happo dependencies
    
    * build: add happo configuration
    
    * ci: remove obsolete ignore-pattern flag for e2e
    
    * ci: update node-ci to trigger happo build
    
    This will build storybook also so it removed the redundant call
    
    * ci: require passing Happo build
    
    * ci: update build step name to note building storybook
    
    * ci: add edge and firefox testing to happo run
    
    * docs: add note about setting happo key and secret

---
+++ .happo.js
@@ -0,0 +1,23 @@
+const { RemoteBrowserTarget } = require('happo.io')
+const happoPluginStorybook = require('happo-plugin-storybook')
+
+module.exports = {
+  apiKey: process.env.HAPPO_API_KEY,
+  apiSecret: process.env.HAPPO_API_SECRET,
+  targets: {
+    chrome: new RemoteBrowserTarget('chrome', {
+      viewport: '1024x768',
+    }),
+    edge: new RemoteBrowserTarget('edge', {
+      viewport: '1024x768',
+    }),
+    firefox: new RemoteBrowserTarget('firefox', {
+      viewport: '1024x768',
+    }),
+  },
+  plugins: [
+    happoPluginStorybook({
+      outputDir: 'storybook-static',
+    }),
+  ],
+}
