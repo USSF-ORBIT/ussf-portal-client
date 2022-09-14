e37be0552cf9ad1dbeb84c1679e41e94b62fa636
Jacob Capps <99674188+jcbcapps@users.noreply.github.com>
2022-09-14 11:49:08 -0500

    feat: Add Light / Dark mode toggle to the portal (#766)
    * Add ThemeToggle component
    
    * Update styles and add mock logic
    
    * Update styles
    
    * Add test
    
    * Add test to bump up coverage
    
    * Add to storybook
    
    * Update localStorage with theme
    
    * Update to use useTheme
    
    * Update text color

---
+++ src/components/ThemeToggle/ThemeToggle.test.tsx
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import ThemeToggle from './ThemeToggle'
+
+describe('ThemeToggle component', () => {
+  it('renders the component', () => {
+    render(<ThemeToggle />)
+
+    const toggleBtn = screen.getByTestId('theme-toggle')
+    expect(toggleBtn).toBeVisible()
+
+    userEvent.click(toggleBtn)
+    expect(screen.getAllByText('light mode')).toHaveLength(1)
+  })
+})
