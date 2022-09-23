ae1aea73f233683a77941ba67b83d572ff85d505
John Gedeon <john@truss.works>
2022-09-23 10:25:39 -0700

    feat: call mutation to save theme setting when toggle is clicked

--- src/components/ThemeToggle/ThemeToggle.test.tsx
+++ src/components/ThemeToggle/ThemeToggle.test.tsx
@@ -2,14 +2,18 @@
  * @jest-environment jsdom
  */
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { waitFor, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import ThemeToggle from './ThemeToggle'
+import { ThemeProvider } from 'next-themes'
 import * as analyticsHooks from 'stores/analyticsContext'
+import { mockUseTheme, renderWithAuthAndApollo } from '../../testHelpers'
+import { editThemeMock } from '../../__fixtures__/operations/editTheme'
+
+import ThemeToggle from './ThemeToggle'
 
 describe('ThemeToggle component', () => {
   it('renders the component', () => {
-    render(<ThemeToggle />)
+    renderWithAuthAndApollo(<ThemeToggle />, {}, editThemeMock)
 
     const toggleBtn = screen.getByTestId('theme-toggle')
     expect(toggleBtn).toBeVisible()
@@ -19,8 +23,9 @@ describe('ThemeToggle component', () => {
   })
 })
 
-describe('calling trackEvent function', () => {
+describe('calling hook functions', () => {
   const mockTrackEvents = jest.fn()
+  const { setItemMock, getItemMock } = mockUseTheme()
 
   beforeEach(() => {
     // mock out the return of useAnalytics since we just need to check that it's called not what it does
@@ -28,7 +33,17 @@ describe('calling trackEvent function', () => {
       return { push: jest.fn(), trackEvent: mockTrackEvents }
     })
 
-    render(<ThemeToggle />)
+    // reset the mocks
+    setItemMock.mockReset()
+    getItemMock.mockReset()
+
+    renderWithAuthAndApollo(
+      <ThemeProvider enableSystem={false}>
+        <ThemeToggle />
+      </ThemeProvider>,
+      {},
+      editThemeMock
+    )
   })
 
   it('calls trackEvent when the toggle is clicked', () => {
@@ -40,6 +55,28 @@ describe('calling trackEvent function', () => {
       'Dark mode',
       'Click on light/dark mode toggle',
       'Light/Dark mode toggle'
+    )
+  })
+
+  it('calls setTheme when the toggle is clicked', () => {
+    const toggle = screen.getByTestId('theme-toggle')
+    userEvent.click(toggle)
+    expect(setItemMock).toHaveBeenCalledTimes(1)
+    expect(setItemMock).toHaveBeenCalledWith('theme', 'dark')
+
+    userEvent.click(toggle)
+    expect(setItemMock).toHaveBeenCalledTimes(2)
+    expect(setItemMock).toHaveBeenLastCalledWith('theme', 'light')
+  })
+
+  it('calls handleEditThemeMutation when the toggle is clicked', async () => {
+    const toggle = screen.getByTestId('theme-toggle')
+    userEvent.click(toggle)
+
+    // only need to check that the function was called the expected number of times
+    // the expected parameters are defined in the mock
+    await waitFor(() =>
+      expect(editThemeMock[0].newData).toHaveBeenCalledTimes(3)
     )
   })
 })
--- src/testHelpers.tsx
+++ src/testHelpers.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, RenderOptions } from '@testing-library/react'
+import { MockedProvider, MockedResponse } from '@apollo/client/testing'
 
 import { testUser1 } from './__fixtures__/authUsers'
 import { AuthContext, AuthContextType } from 'stores/authContext'
@@ -25,7 +26,7 @@ export const defaultMockAuthContext = {
 }
 
 /** Renders the component inside of an already-authenticated AuthProvider */
-/** You can pass custom context values (user, mock functions) as needed */
+/** You can pass custom auth context values (user, mock functions) as needed */
 export const renderWithAuth = (
   component: React.ReactElement,
   value: Partial<AuthContextType> = {}
@@ -35,9 +36,68 @@ export const renderWithAuth = (
     ...value,
   }
 
-  return render(
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  )
+
+  return render(component, { wrapper })
+}
+
+/** Renders the component inside of an already-authenticated AuthProvider and a MockedProvider*/
+/** You can pass custom auth context values (user, mock functions) as needed */
+/** You can pass graphql mocks as needed */
+export const renderWithAuthAndApollo = (
+  component: React.ReactElement,
+  value: Partial<AuthContextType> = {},
+  mocks: readonly MockedResponse<Record<string, unknown>>[] = []
+) => {
+  const contextValue = {
+    ...defaultMockAuthContext,
+    ...value,
+  }
+
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
     <AuthContext.Provider value={contextValue}>
-      {component}
+      <MockedProvider
+        mocks={mocks}
+        addTypename={false}
+        defaultOptions={{
+          watchQuery: { fetchPolicy: 'no-cache' },
+          query: { fetchPolicy: 'no-cache' },
+        }}>
+        {children}
+      </MockedProvider>
     </AuthContext.Provider>
   )
+
+  return render(component, { wrapper })
+}
+
+/** sets up some mocks for the useTheme hook **/
+export const mockUseTheme = () => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  })
+
+  const localStorageMock: { [key: string]: string } = {}
+  const getItemMock = jest.fn((key: string) => localStorageMock[key])
+  const setItemMock = jest.fn((key: string, value: string) => {
+    localStorageMock[key] = value
+  })
+  global.Storage.prototype.getItem = getItemMock
+  // eslint-disable-next-line security/detect-object-injection
+  global.Storage.prototype.setItem = setItemMock
+
+  // eslint-disable-next-line security/detect-object-injection
+  return { getItemMock, setItemMock }
 }
