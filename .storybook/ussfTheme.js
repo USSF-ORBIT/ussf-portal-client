ab1967011b7a25e0f2078714c1313d526820de89
Shauna Keating <shauna@truss.works>
2022-10-05 14:23:20 -0400

    makes background colors spaceforce colors

--- .storybook/ussfTheme.js
+++ .storybook/ussfTheme.js
@@ -6,5 +6,11 @@ export default create({
   brandUrl: 'https://github.com/USSF-ORBIT/ussf-portal-client',
   brandImage: '/img/ussf-logo-short.svg',
   brandTarget: '_self',
+
   fontBase: '"Sharp Sans", "Trebuchet MS", sans-serif',
+
+  barBg: '#0e163b', //adddons and canvas toolbar bg
+  appContentBg: '#0e163b', //addons  panel background
+  appBg: '#070b1e', //sidebar background
+  barTextColor: '#a7afd4', //adddons and canvas toolbar text
 })
