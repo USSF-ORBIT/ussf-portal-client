a6a8c1384a9f17eb55def8350a0c7238267f0740
Abigail Young <abbyoung@gmail.com>
2022-10-14 15:07:56 -0700

    dev: Dark mode support for alerts (#791)
    * news widget and add section widget dark mode
    
    * Add dark mode suppoert for sites and apps based on types and alpha view
    
    * fix double custom collection icon
    
    * Add dark mode and theme support for alerts
    
    * update comment
    
    * fix react uswds webkit prefixing in storybook
    
    * fix formatting

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+import { Alert } from '@trussworks/react-uswds'
+
+export default {
+  title: 'Components/Alert',
+  component: Alert,
+  decorators: [
+    (Story) => (
+      <div className="sfds">
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta
+
+const successMsg = 'Successful alert'
+const errorMsg = 'Error alert'
+const warningMsg = 'Warning alert'
+const infoMsg = 'Info alert'
+
+export const AlertSuccess = () => (
+  <Alert headingLevel="h1" type="success">
+    {successMsg}
+  </Alert>
+)
+
+export const AlertWarning = () => (
+  <Alert headingLevel="h1" type="warning">
+    {warningMsg}
+  </Alert>
+)
+
+export const AlertError = () => (
+  <Alert headingLevel="h1" type="error">
+    {errorMsg}
+  </Alert>
+)
+
+export const AlertInfo = () => (
+  <Alert headingLevel="h1" type="info">
+    {infoMsg}
+  </Alert>
+)
