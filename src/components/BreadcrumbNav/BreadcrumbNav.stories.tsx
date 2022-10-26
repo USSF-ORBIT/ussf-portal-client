7a06e1a187126384b93bd60628804a81a4a52014
Shauna Keating <shauna@truss.works>
2022-10-26 14:31:27 -0400

    assigns white to arrow icons and text in breadcrumbs

--- src/components/BreadcrumbNav/BreadcrumbNav.stories.tsx
+++ src/components/BreadcrumbNav/BreadcrumbNav.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import type { Meta } from '@storybook/react'
 
 import BreadcrumbNav from './BreadcrumbNav'
+import { background } from '@storybook/theming'
 
 export default {
   title: 'Components/BreadcrumbNav',
