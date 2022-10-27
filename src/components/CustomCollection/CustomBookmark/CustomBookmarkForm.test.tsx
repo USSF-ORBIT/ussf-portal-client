80b1ef99debafe48ea34662ce67a8985b8d22b91
John Gedeon <john@truss.works>
2022-10-27 11:01:45 -0700

    test: update await on userEvent for CustomBookmarkForm tests

--- src/components/CustomCollection/CustomBookmark/CustomBookmarkForm.test.tsx
+++ src/components/CustomCollection/CustomBookmark/CustomBookmarkForm.test.tsx
@@ -8,31 +8,30 @@ import { axe } from 'jest-axe'
 import userEvent from '@testing-library/user-event'
 import { CustomBookmarkForm } from './CustomBookmarkForm'
 
+const testHandlers = {
+  onSave: jest.fn(),
+  onCancel: jest.fn(),
+  onDelete: jest.fn(),
+}
+
+const mockNameRef = createRef<HTMLInputElement>()
+const mockUrlRef = createRef<HTMLInputElement>()
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
 describe('CustomBookmarkForm component', () => {
-  const testHandlers = {
-    onSave: jest.fn(),
-    onCancel: jest.fn(),
-  }
-
-  let html: RenderResult
-
-  beforeEach(async () => {
-    const mockNameRef = createRef<HTMLInputElement>()
-    const mockUrlRef = createRef<HTMLInputElement>()
-
-    html = render(
+  it('renders the CustomBookmarkForm component', async () => {
+    const html = render(
       <CustomBookmarkForm
-        onSave={testHandlers.onSave}
-        onCancel={testHandlers.onCancel}
+        {...testHandlers}
         nameInputRef={mockNameRef}
         urlInputRef={mockUrlRef}
       />
     )
-    await screen.findByLabelText('Name')
-  })
-
-  it('renders the CustomBookmarkForm component', async () => {
-    expect(screen.getByLabelText('Name')).toBeInTheDocument()
+    const label = await screen.findByLabelText('Name')
+    expect(label).toBeInTheDocument()
     expect(screen.getByLabelText('URL')).toBeInTheDocument()
     expect(
       screen.getByRole('button', { name: 'Save custom link' })
@@ -40,48 +39,67 @@ describe('CustomBookmarkForm component', () => {
   })
 
   it('validates the bookmarkLabel field', async () => {
-    // Input: On load, empty
-    // Result: Invalid
-    const labelInput = screen.getByLabelText('Name')
-    expect(labelInput).toBeInvalid()
+    const user = userEvent.setup()
+    const html = render(
+      <CustomBookmarkForm
+        {...testHandlers}
+        nameInputRef={mockNameRef}
+        urlInputRef={mockUrlRef}
+      />,
+      { legacyRoot: true }
+    )
+
+    // Input: On load, empty, untouched
+    // Result: Valid
+    const labelInput = await screen.findByLabelText('Name')
+    expect(labelInput).toBeValid()
 
     // Input: String
     // Result: Valid
-    userEvent.type(labelInput, 'My Custom Link')
-    await waitFor(() => expect(labelInput).toBeValid())
+    await user.type(labelInput, 'My Custom Link')
+    expect(labelInput).toBeValid()
 
     // Input: Empty
     // Result: Invalid
-    userEvent.clear(labelInput)
-    await waitFor(() => expect(labelInput).toBeInvalid())
+    await user.clear(labelInput)
+    expect(labelInput).toBeInvalid()
   })
 
   it('validates the bookmarkUrl field', async () => {
-    // Input: On load, empty
-    // Result: Invalid
-    const urlInput = screen.getByLabelText('URL')
-    expect(urlInput).toBeInvalid()
+    const user = userEvent.setup()
+    const html = render(
+      <CustomBookmarkForm
+        {...testHandlers}
+        nameInputRef={mockNameRef}
+        urlInputRef={mockUrlRef}
+      />
+    )
+
+    // Input: On load, empty, untouched
+    // Result: Valid
+    const urlInput = await screen.findByLabelText('URL')
+    expect(urlInput).toBeValid()
 
     // Input: String, non-valid URL
     // Result: Invalid
-    userEvent.type(urlInput, 'invalid input')
-    await waitFor(() => expect(urlInput).toBeInvalid())
+    await user.type(urlInput, 'invalid input')
+    expect(urlInput).toBeInvalid()
 
-    userEvent.clear(urlInput)
+    await user.clear(urlInput)
 
     // Input: String, valid url with http://
     // Result: Valid
-    userEvent.type(urlInput, 'http://example.com')
-    await waitFor(() => expect(urlInput).toBeValid())
+    await user.type(urlInput, 'http://example.com')
+    expect(urlInput).toBeValid()
 
-    userEvent.clear(urlInput)
+    await user.clear(urlInput)
 
     // Input: String, valid url with https://
     // Result: Valid
-    userEvent.type(urlInput, 'https://example.com')
-    await waitFor(() => expect(urlInput).toBeValid())
+    await user.type(urlInput, 'https://example.com')
+    expect(urlInput).toBeValid()
 
-    userEvent.clear(urlInput)
+    await user.clear(urlInput)
 
     // Input: String, valid url with no scheme
     // Result: Valid
@@ -90,144 +108,154 @@ describe('CustomBookmarkForm component', () => {
     //          Because we have custom validation that does not conform to
     //          built-in url checking, `checkValidity()` returns false in this scenario,
     //          even though aria-invalid is false and our input is valid.
-    userEvent.type(urlInput, 'example.com')
-    await waitFor(() =>
-      expect(urlInput).toHaveAttribute('aria-invalid', 'false')
-    )
+    await user.type(urlInput, 'example.com')
+    expect(urlInput).toHaveAttribute('aria-invalid', 'false')
   })
 
   it('can cancel the form', async () => {
+    const user = userEvent.setup()
+    const html = render(
+      <CustomBookmarkForm
+        {...testHandlers}
+        nameInputRef={mockNameRef}
+        urlInputRef={mockUrlRef}
+      />
+    )
+
     // Input:
     //    Name: My Custom Link
     //    URL: example.com
     // Action: Cancel
     // Result: onCancel is called
-    const labelInput = screen.getByLabelText('Name')
-    userEvent.type(labelInput, 'My Custom Link')
-    await waitFor(() => expect(labelInput).toBeValid())
+    const labelInput = await screen.findByLabelText('Name')
+    await user.type(labelInput, 'My Custom Link')
+    expect(labelInput).toBeValid()
 
     const urlInput = screen.getByLabelText('URL')
-    userEvent.type(urlInput, 'example.com')
-    await waitFor(() =>
-      expect(urlInput).toHaveAttribute('aria-invalid', 'false')
-    )
+    await user.type(urlInput, 'example.com')
+    expect(urlInput).toHaveAttribute('aria-invalid', 'false')
 
-    userEvent.click(screen.getByRole('button', { name: 'Cancel' }))
+    await user.click(screen.getByRole('button', { name: 'Cancel' }))
 
-    await waitFor(() => expect(testHandlers.onCancel).toHaveBeenCalledTimes(1))
+    expect(testHandlers.onCancel).toHaveBeenCalledTimes(1)
   })
 
   it('has no a11y violations', async () => {
+    const html = render(
+      <CustomBookmarkForm
+        {...testHandlers}
+        nameInputRef={mockNameRef}
+        urlInputRef={mockUrlRef}
+      />
+    )
+
+    await screen.findByLabelText('Name')
     expect(await axe(html.container)).toHaveNoViolations()
   })
 })
 
 describe('CustomBookmarkForm component, Add Custom Link', () => {
-  const testHandlers = {
-    onSave: jest.fn(),
-    onCancel: jest.fn(),
-  }
-
-  let html: RenderResult
-
-  beforeEach(async () => {
-    const mockNameRef = createRef<HTMLInputElement>()
-    const mockUrlRef = createRef<HTMLInputElement>()
-
-    html = render(
+  it('can save a valid form', async () => {
+    const user = userEvent.setup()
+    const html = render(
       <CustomBookmarkForm
-        onSave={testHandlers.onSave}
-        onCancel={testHandlers.onCancel}
+        {...testHandlers}
         nameInputRef={mockNameRef}
         urlInputRef={mockUrlRef}
       />
     )
-    await screen.findByLabelText('Name')
-  })
-  it('can save a valid form', async () => {
     // Input:
     //    Name: My Custom Link
     //    URL: https://example.com
     // Action: Save
     // Result: onSave is called
-    const labelInput = screen.getByLabelText('Name')
-    userEvent.type(labelInput, 'My Custom Link')
-    await waitFor(() => expect(labelInput).toBeValid())
+    const labelInput = await screen.findByLabelText('Name')
+    await user.type(labelInput, 'My Custom Link')
+    expect(labelInput).toBeValid()
 
     const urlInput = screen.getByLabelText('URL')
-    userEvent.type(urlInput, 'https://example.com')
-    await waitFor(() => expect(labelInput).toBeValid())
+    await user.type(urlInput, 'https://example.com')
+    expect(labelInput).toBeValid()
 
-    userEvent.click(screen.getByRole('button', { name: 'Save custom link' }))
+    await user.click(screen.getByRole('button', { name: 'Save custom link' }))
 
-    await waitFor(() => {
-      expect(testHandlers.onSave).toHaveBeenCalledWith(
-        'https://example.com',
-        'My Custom Link'
-      )
-    })
-    expect(screen.getByLabelText('Name')).toHaveValue('')
+    expect(testHandlers.onSave).toHaveBeenCalledWith(
+      'https://example.com',
+      'My Custom Link'
+    )
+    await waitFor(() => expect(screen.getByLabelText('Name')).toHaveValue(''))
     expect(screen.getByLabelText('URL')).toHaveValue('')
   })
 
   it('reverts the value of text when cancelling', async () => {
-    const labelInput = screen.getByLabelText('Name')
-    userEvent.type(labelInput, 'My Custom Link')
+    const user = userEvent.setup()
+    const html = render(
+      <CustomBookmarkForm
+        {...testHandlers}
+        nameInputRef={mockNameRef}
+        urlInputRef={mockUrlRef}
+      />
+    )
+    const labelInput = await screen.findByLabelText('Name')
+    await user.type(labelInput, 'My Custom Link')
     const urlInput = screen.getByLabelText('URL')
 
-    userEvent.type(urlInput, 'http://www.example.com')
-    userEvent.click(screen.getByRole('button', { name: 'Cancel' }))
+    await user.type(urlInput, 'http://www.example.com')
+    await user.click(screen.getByRole('button', { name: 'Cancel' }))
 
-    await waitFor(() => expect(testHandlers.onCancel).toHaveBeenCalled())
+    expect(testHandlers.onCancel).toHaveBeenCalled()
     expect(screen.getByLabelText('Name')).toHaveValue('')
     expect(screen.getByLabelText('URL')).toHaveValue('')
   })
 
   it('has no a11y violations', async () => {
-    expect(await axe(html.container)).toHaveNoViolations()
-  })
-})
-
-describe('CustomBookmarkForm component, Edit Custom Link', () => {
-  const testHandlers = {
-    onSave: jest.fn(),
-    onCancel: jest.fn(),
-    onDelete: jest.fn(),
-  }
-  let html: RenderResult
-  beforeEach(async () => {
-    const mockNameRef = createRef<HTMLInputElement>()
-    const mockUrlRef = createRef<HTMLInputElement>()
-
-    html = render(
+    const html = render(
       <CustomBookmarkForm
-        onSave={testHandlers.onSave}
-        onCancel={testHandlers.onCancel}
-        onDelete={testHandlers.onDelete}
+        {...testHandlers}
         nameInputRef={mockNameRef}
         urlInputRef={mockUrlRef}
       />
     )
     await screen.findByLabelText('Name')
+    expect(await axe(html.container)).toHaveNoViolations()
   })
+})
+
+describe('CustomBookmarkForm component, Edit Custom Link', () => {
   it('can delete a bookmark using the form', async () => {
+    const user = userEvent.setup()
+    const html = render(
+      <CustomBookmarkForm
+        {...testHandlers}
+        nameInputRef={mockNameRef}
+        urlInputRef={mockUrlRef}
+      />
+    )
     // Input: Existing link, no input
     // Action: Delete
     // Result: onDelete is called
-    const labelInput = screen.getByLabelText('Name')
-    userEvent.type(labelInput, 'My Custom Link')
-    await waitFor(() => expect(labelInput).toBeValid())
+    const labelInput = await screen.findByLabelText('Name')
+    await user.type(labelInput, 'My Custom Link')
+    expect(labelInput).toBeValid()
 
     const urlInput = screen.getByLabelText('URL')
-    userEvent.type(urlInput, 'https://example.com')
-    await waitFor(() => expect(labelInput).toBeValid())
+    await user.type(urlInput, 'https://example.com')
+    expect(labelInput).toBeValid()
 
-    userEvent.click(screen.getByRole('button', { name: 'Delete' }))
+    await user.click(screen.getByRole('button', { name: 'Delete' }))
 
-    await waitFor(() => expect(testHandlers.onDelete).toHaveBeenCalledTimes(1))
+    expect(testHandlers.onDelete).toHaveBeenCalledTimes(1)
   })
 
   it('has no a11y violations', async () => {
+    const html = render(
+      <CustomBookmarkForm
+        {...testHandlers}
+        nameInputRef={mockNameRef}
+        urlInputRef={mockUrlRef}
+      />
+    )
+    await screen.findByLabelText('Name')
     expect(await axe(html.container)).toHaveNoViolations()
   })
 })
