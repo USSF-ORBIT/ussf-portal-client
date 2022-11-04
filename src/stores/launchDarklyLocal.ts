f8331a6d7a3dc04c9b1e2037e1dbe2dd55993af7
John Gedeon <john@truss.works>
2022-11-04 15:02:47 -0700

    docs: add a note about the new launchDarklyLocal.ts file

--- src/stores/launchDarklyLocal.ts
+++ src/stores/launchDarklyLocal.ts
@@ -1,1 +1,6 @@
+/*
+ * This file is only utilized when LAUNCHDARKLY_SDK_CLIENT_SIDE_ID is set to localfile
+ * Add flags as needed and set them, though you will likely need to restart your dev
+ * server to see changes.
+ */
 export default { darkModeToggle: true }
