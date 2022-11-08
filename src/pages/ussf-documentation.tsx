b46e9dd535eceb6ed1975d574d658f8300c16647
Jacob Capps <jcapps@truss.works>
2022-11-08 15:20:08 -0600

    Create page

---
+++ src/pages/ussf-documentation.tsx
@@ -0,0 +1,13 @@
+import { withDefaultLayout } from 'layout/DefaultLayout/DefaultLayout'
+
+const USSFDocumentation = () => {
+  return (
+    <div>
+      <h2>Official USSF documentation</h2>
+    </div>
+  )
+}
+
+export default USSFDocumentation
+
+USSFDocumentation.getLayout = withDefaultLayout
