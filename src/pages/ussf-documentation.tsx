44b117021add84704be8785c0791c366731915bb
Jacob Capps <jcapps@truss.works>
2022-11-09 11:29:16 -0600

    Add docs to Accordion

--- src/pages/ussf-documentation.tsx
+++ src/pages/ussf-documentation.tsx
@@ -1,15 +1,54 @@
 import React from 'react'
 import { Accordion } from '@trussworks/react-uswds'
 import { AccordionItemProps } from '@trussworks/react-uswds/lib/components/Accordion/Accordion'
+import LinkTo from 'components/util/LinkTo/LinkTo'
 import { withDefaultLayout } from 'layout/DefaultLayout/DefaultLayout'
+import styles from 'styles/pages/ussf-documentation.module.scss'
 
 const USSFDocumentation = () => {
   const testItems: AccordionItemProps[] = [
     {
-      title: 'First',
-      content: <div>This is a test</div>,
+      title: 'Essential Reading',
+      content: (
+        <div className={styles.documentation}>
+          <LinkTo
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://www.spaceforce.mil/Portals/1/Space%20Capstone%20Publication_10%20Aug%202020.pdf">
+            Space Capstone Publication: Spacepower. Doctrine for Space Forces
+          </LinkTo>
+
+          <LinkTo
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://media.defense.gov/2020/Nov/09/2002531998/-1/-1/0/CSO%20PLANNING%20GUIDANCE.PDF">
+            CSO’s Planning Guidance
+          </LinkTo>
+
+          <LinkTo
+            target="_blank"
+            rel="noreferrer noopener"
+            href="/uploads/Guardian Ideal - FINAL - 1600 17Sept21.pdf">
+            Guardian Ideal
+          </LinkTo>
+
+          <LinkTo
+            target="_blank"
+            rel="noreferrer noopener"
+            href="/uploads/US Space Force Enlisted Rank Insig Info Sheet (1).pdf">
+            USSF Enlisted Rank and Insignia
+          </LinkTo>
+
+          <LinkTo
+            target="_blank"
+            rel="noreferrer noopener"
+            href="/uploads/USSF Health Wellness and Fitness Memo dated 16Mar22.pdf">
+            USSF/S1 Health, Wellness and Fitness Memo (16 MAR 2022)
+          </LinkTo>
+        </div>
+      ),
       expanded: false,
-      id: 'thisistheid',
+      id: 'essential-reading',
       headingLevel: 'h3',
     },
   ]
