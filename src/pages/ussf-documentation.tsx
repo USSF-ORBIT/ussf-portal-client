1e1d1b0d1bd75bbdeff8ffec1f661b50ee809867
Jacob Capps <jcapps@truss.works>
2022-11-09 09:56:44 -0600

    Add type

--- src/pages/ussf-documentation.tsx
+++ src/pages/ussf-documentation.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Accordion } from '@trussworks/react-uswds'
+import { AccordionItemProps } from '@trussworks/react-uswds/lib/components/Accordion/Accordion'
 import { withDefaultLayout } from 'layout/DefaultLayout/DefaultLayout'
 
 const USSFDocumentation = () => {
+  const testItems: AccordionItemProps[] = [
+    {
+      title: 'First',
+      content: <div>This is a test</div>,
+      expanded: false,
+      id: 'thisistheid',
+      headingLevel: 'h3',
+    },
+  ]
+
   return (
     <div>
       <h2>Official USSF documentation</h2>
+      <Accordion bordered={true} items={testItems} />
     </div>
   )
 }
