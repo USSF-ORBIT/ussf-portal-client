5e5c147fa06ed1a71fbea9b1fd6337905a308054
Jacob Capps <jcapps@truss.works>
2022-11-10 09:50:18 -0600

    Add Category and change display order for labels and tags

--- src/components/SingleArticle/SingleArticle.tsx
+++ src/components/SingleArticle/SingleArticle.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { DocumentRenderer } from '@keystone-6/document-renderer'
 import { Tag } from '@trussworks/react-uswds'
 import styles from './SingleArticle.module.scss'
-import { Label } from 'components/Tag/Tag'
+import { Category, Label } from 'components/Tag/Tag'
 import type { ArticleRecord } from 'types'
+import { CONTENT_CATEGORIES } from 'constants/index'
 import colors from 'styles/sfds/colors.module.scss'
 
 export const SingleArticle = ({ article }: { article: ArticleRecord }) => {
@@ -31,14 +32,11 @@ export const SingleArticle = ({ article }: { article: ArticleRecord }) => {
     <article className={styles.SingleArticle}>
       <div>
         <div className={styles.tagAndLabelContainer}>
-          {tags &&
-            tags.map((tag) => {
-              return (
-                <Tag key={`${tag.id}`} background={colors['theme-mars-base']}>
-                  {tag.name}
-                </Tag>
-              )
-            })}
+          {/* 
+            This will always be Internal News, because External News refers to 
+            the RSS feed, which links to an article on a separate domain.
+          */}
+          <Category category={CONTENT_CATEGORIES.INTERNAL_NEWS} />
 
           {labels &&
             labels.map((label) => {
@@ -46,6 +44,15 @@ export const SingleArticle = ({ article }: { article: ArticleRecord }) => {
                 <Label type={label.type} key={`${label.id}`}>
                   {label.name}
                 </Label>
+              )
+            })}
+
+          {tags &&
+            tags.map((tag) => {
+              return (
+                <Tag key={`${tag.id}`} background={colors['theme-mars-base']}>
+                  {tag.name}
+                </Tag>
               )
             })}
         </div>
