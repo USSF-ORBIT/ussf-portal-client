9baeccf6e9ca8a3d47d3a8af7a73de881a19992a
John Gedeon <john@truss.works>
2022-11-30 14:35:43 -0800

    test: mock out rss feed for NewsWidget story

--- src/components/NewsWidget/NewsWidget.stories.tsx
+++ src/components/NewsWidget/NewsWidget.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Meta } from '@storybook/react'
 
-import NewsWidget from './NewsWidget'
+import { mockRssFeedTwo } from '__mocks__/news-rss'
+import NewsWidget, { RSS_URL } from './NewsWidget'
 
 type StorybookArgTypes = {
   onRemove: () => void
@@ -20,6 +21,18 @@ export default {
       </div>
     ),
   ],
+  parameters: {
+    mockData: [
+      {
+        url: RSS_URL,
+        method: 'GET',
+        status: 200,
+        response: () => {
+          return mockRssFeedTwo
+        },
+      },
+    ],
+  },
 } as Meta
 
 export const SpaceForceRSS = (argTypes: StorybookArgTypes) => (
--- src/components/NewsWidget/NewsWidget.tsx
+++ src/components/NewsWidget/NewsWidget.tsx
@@ -14,7 +14,7 @@ import RemoveSectionModal from 'components/modals/RemoveSectionModal'
 import { useAnalytics } from 'stores/analyticsContext'
 
 // Load 2 items
-const RSS_URL = `${SPACEFORCE_NEWS_RSS_URL}&max=2`
+export const RSS_URL = `${SPACEFORCE_NEWS_RSS_URL}&max=2`
 
 const NewsWidget = ({ onRemove }: { onRemove: () => void }) => {
   const { items, fetchItems } = useRSSFeed(RSS_URL)
