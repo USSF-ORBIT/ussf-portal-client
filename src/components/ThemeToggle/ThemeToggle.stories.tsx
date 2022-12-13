59618903d2e65c12d1a61cceb8f963abcdb2cfbb
John Gedeon <john@truss.works>
2022-12-13 15:51:59 -0800

    fix: add mock user for ThemeToggle story (#889)

--- src/components/ThemeToggle/ThemeToggle.stories.tsx
+++ src/components/ThemeToggle/ThemeToggle.stories.tsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import { Meta, StoryObj } from '@storybook/react'
 
+import { testUser1 } from '../../__fixtures__/authUsers'
+
 import ThemeToggle from './ThemeToggle'
+
+import { AuthContext } from 'stores/authContext'
+
+const mockContext = {
+  user: testUser1,
+  setUser: () => {
+    return
+  },
+  logout: () => {
+    return
+  },
+  login: () => {
+    return
+  },
+}
 
 export default {
   title: 'Components/ThemeToggle',
   component: ThemeToggle,
   decorators: [
     (Story) => (
-      <div className="sfds">
-        <Story />
-      </div>
+      <AuthContext.Provider value={mockContext}>
+        <div className="sfds">
+          <Story />
+        </div>
+      </AuthContext.Provider>
     ),
   ],
 } as Meta
 
-export const Example: StoryObj = {}
+export const WithUser = () => <ThemeToggle />
+
+export const NoUser = () => <ThemeToggle />
+
+NoUser.decorators = [
+  (Story: any) => (
+    <AuthContext.Provider value={{ ...mockContext, user: null }}>
+      <div className="sfds">
+        <Story />
+      </div>
+    </AuthContext.Provider>
+  ),
+]
