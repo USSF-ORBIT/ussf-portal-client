5603dd63234ea6641998a7be628358ef4713fd26
Shauna Keating <shauna@truss.works>
2022-12-21 14:38:58 -0500

    adjusts markup to inlcude fontawesome sun and moon icons

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -6,6 +6,19 @@ import { useUser } from 'hooks/useUser'
 import { useEditThemeMutation } from 'operations/portal/mutations/editTheme.g'
 import { SessionUser } from 'types'
 import { useGetThemeQuery } from 'operations/portal/queries/getTheme.g'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+
+const lightTheme = (
+  <>
+    <FontAwesomeIcon icon={faSun} /> light
+  </>
+)
+const darkTheme = (
+  <>
+    <FontAwesomeIcon icon={faMoon} /> dark
+  </>
+)
 
 const ThemeToggle = () => {
   const { data } = useGetThemeQuery()
@@ -66,7 +79,7 @@ const ThemeToggle = () => {
       }}
       className={styles.toggleButton}
       data-testid="theme-toggle">
-      {theme === 'light' ? 'dark' : 'light'} mode
+      {theme === lightTheme ? darkTheme : lightTheme} mode
     </button>
   )
 }
