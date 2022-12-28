b161ac8d5e53394ef0583c7cf6794dc5a716c7e2
Jacob Capps <jcapps@truss.works>
2022-12-28 13:37:16 -0600

    Create modalContext

---
+++ src/stores/modalContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from 'react'
+
+type ModalContextType = {
+  toggleDisplay: boolean
+  component: React.ReactNode | null
+  handleToggleDisplayModal: () => void
+}
+
+const ModalContext = createContext<ModalContextType>({
+  toggleDisplay: false,
+  component: null,
+  handleToggleDisplayModal: () => {
+    return
+  },
+})
+
+export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
+  const [toggleDisplay, setToggleDisplay] = useState(false)
+
+  const handleToggleDisplayModal = () => {
+    setToggleDisplay((prevDisplayState) => !prevDisplayState)
+  }
+
+  const context = {
+    toggleDisplay,
+    component: null,
+    handleToggleDisplayModal,
+  }
+
+  return (
+    <ModalContext.Provider value={context}>{children}</ModalContext.Provider>
+  )
+}
