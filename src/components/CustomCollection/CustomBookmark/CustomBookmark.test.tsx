fb7f3f493dbdf66ff1d451170994c594b905237e
Jacob Capps <jcapps@truss.works>
2023-01-09 15:09:23 -0600

    Update tests

--- src/components/CustomCollection/CustomBookmark/CustomBookmark.test.tsx
+++ src/components/CustomCollection/CustomBookmark/CustomBookmark.test.tsx
@@ -65,82 +65,38 @@ describe('CustomBookmark component', () => {
     expect(screen.getByRole('link')).toHaveTextContent(testBookmarkNoLabel.url)
   })
 
-  // it('can save the bookmark', async () => {
-  //   const user = userEvent.setup()
+  it('can save the bookmark', async () => {
+    const user = userEvent.setup()
+    const mockUpdateModalId = jest.fn()
+    const mockUpdateModalText = jest.fn()
+    const mockUpdateWidget = jest.fn()
+    const mockUpdateBookmark = jest.fn()
 
-  //   renderWithModalRoot(
-  //     <CustomBookmark
-  //       bookmark={testBookmark}
-  //       widgetId={testWidgetId}
-  //       collectionTitle={testCollectionTitle}
-  //     />
-  //   )
+    renderWithModalRoot(
+      <CustomBookmark
+        bookmark={testBookmark}
+        widgetId={testWidgetId}
+        collectionTitle={testCollectionTitle}
+      />,
+      {
+        updateModalId: mockUpdateModalId,
+        updateModalText: mockUpdateModalText,
+        updateWidget: mockUpdateWidget,
+        updateBookmark: mockUpdateBookmark,
+      }
+    )
 
-  //   const editButton = await screen.findByRole('button', {
-  //     name: 'Edit this link',
-  //   })
-  //   await user.click(editButton)
-  //   expect(
-  //     screen.getByRole('dialog', { name: 'Edit custom link' })
-  //   ).not.toHaveClass('is-hidden')
+    const editButton = await screen.findByRole('button', {
+      name: 'Edit this link',
+    })
+    await user.click(editButton)
 
-  //   const saveButton = screen.getByRole('button', { name: 'Save custom link' })
-  //   expect(saveButton).toBeInTheDocument()
-  //   await user.click(saveButton)
-  //   expect(testHandlers.onSave).toHaveBeenCalled()
-  // })
+    expect(mockUpdateModalId).toHaveBeenCalledWith('editCustomLinkModal')
+    expect(mockUpdateModalText).toHaveBeenCalledWith({
+      headingText: 'Edit custom link',
+    })
 
-  // it('can delete the bookmark', async () => {
-  //   const user = userEvent.setup()
-
-  //   renderWithModalRoot(
-  //     <CustomBookmark
-  //       bookmark={testBookmark}
-  //       widgetId={testWidgetId}
-  //       collectionTitle={testCollectionTitle}
-  //     />
-  //   )
-
-  //   const editButton = await screen.findByRole('button', {
-  //     name: 'Edit this link',
-  //   })
-  //   await user.click(editButton)
-  //   expect(
-  //     screen.getByRole('dialog', { name: 'Edit custom link' })
-  //   ).not.toHaveClass('is-hidden')
-
-  //   const deleteButton = screen.getByRole('button', { name: 'Delete' })
-  //   expect(deleteButton).toBeInTheDocument()
-  //   await user.click(deleteButton)
-  //   expect(testHandlers.onDelete).toHaveBeenCalled()
-  // })
-
-  // it('can start editing and cancel', async () => {
-  //   const user = userEvent.setup()
-
-  //   renderWithModalRoot(
-  //     <CustomBookmark
-  //       bookmark={testBookmark}
-  //       widgetId={testWidgetId}
-  //       collectionTitle={testCollectionTitle}
-  //     />
-  //   )
-
-  //   const editButton = await screen.findByRole('button', {
-  //     name: 'Edit this link',
-  //   })
-  //   await user.click(editButton)
-  //   expect(
-  //     screen.getByRole('dialog', { name: 'Edit custom link' })
-  //   ).not.toHaveClass('is-hidden')
-
-  //   const cancelButton = screen.getByRole('button', { name: 'Cancel' })
-  //   expect(cancelButton).toBeInTheDocument()
-  //   await user.click(cancelButton)
-  //   expect(
-  //     screen.getByRole('dialog', { name: 'Edit custom link' })
-  //   ).toHaveClass('is-hidden')
-  //   expect(testHandlers.onSave).not.toHaveBeenCalled()
-  //   expect(testHandlers.onDelete).not.toHaveBeenCalled()
-  // })
+    expect(mockUpdateWidget).toHaveBeenCalled()
+    expect(mockUpdateBookmark).toHaveBeenCalled()
+  })
 })
