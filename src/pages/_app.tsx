8327e39a309da038e39e2409195dbd2b0f244ed4
Jacob Capps <jcapps@truss.works>
2023-01-25 10:14:20 -0600

    Add launchDarklyContext w/ localfile config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,10 @@ import App from 'next/app'
 import Head from 'next/head'
 import type { AppProps, AppContext } from 'next/app'
 import { useRouter } from 'next/router'
-import type { ReactNode, ComponentType } from 'react'
+import type { ReactNode } from 'react'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'next-themes'
-import { withLDProvider } from 'launchdarkly-react-client-sdk'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import 'styles/index.scss'
@@ -20,6 +19,7 @@ import { AuthProvider } from 'stores/authContext'
 import { ModalProvider } from 'stores/modalContext'
 import DefaultLayout from 'layout/DefaultLayout/DefaultLayout'
 import { getAbsoluteUrl } from 'lib/getAbsoluteUrl'
+import { LaunchDarkly } from 'stores/launchDarklyContext'
 
 config.autoAddCss = false
 
@@ -46,128 +46,130 @@ const USSFPortalApp = ({ Component, pageProps, hostname }: Props) => {
     <ApolloProvider client={client}>
       <AnalyticsProvider>
         <AuthProvider>
-          <ThemeProvider enableSystem={false} attribute={'data-color-theme'}>
-            <ModalProvider>
-              <Head>
-                <meta charSet="utf-8" key="charset" />
-                <meta
-                  httpEquiv="X-UA-Compatible"
-                  content="IE=edge"
-                  key="edge"
-                />
-                <meta
-                  name="viewport"
-                  content="width=device-width, initial-scale=1"
-                  key="viewport"
-                />
-                <link rel="canonical" href={canonicalUrl + asPath} />
-                <title>Space Force Portal</title>
-                <link
-                  rel="apple-touch-icon"
-                  sizes="180x180"
-                  href="/apple-touch-icon.png"
-                />
-                <link
-                  rel="icon"
-                  type="image/png"
-                  sizes="32x32"
-                  href="/favicon-32x32.png"
-                />
-                <link
-                  rel="icon"
-                  type="image/png"
-                  sizes="16x16"
-                  href="/favicon-16x16.png"
-                />
-                <link
-                  rel="manifest"
-                  href="/site.webmanifest"
-                  crossOrigin="use-credentials"
-                />
-                <link
-                  rel="mask-icon"
-                  href="/safari-pinned-tab.svg"
-                  color="#5bbad5"
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-Book.woff"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-Book.woff2"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-BookItalic.woff"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-BookItalic.woff2"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-Medium.woff"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-Medium.woff2"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-MediumItalic.woff"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-MediumItalic.woff2"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-Bold.woff"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-Bold.woff2"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-BoldItalic.woff"
-                  as="font"
-                  crossOrigin=""
-                />
-                <link
-                  rel="preload"
-                  href="/assets/fonts/SharpSans-BoldItalic.woff2"
-                  as="font"
-                  crossOrigin=""
-                />
+          <LaunchDarkly>
+            <ThemeProvider enableSystem={false} attribute={'data-color-theme'}>
+              <ModalProvider>
+                <Head>
+                  <meta charSet="utf-8" key="charset" />
+                  <meta
+                    httpEquiv="X-UA-Compatible"
+                    content="IE=edge"
+                    key="edge"
+                  />
+                  <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                    key="viewport"
+                  />
+                  <link rel="canonical" href={canonicalUrl + asPath} />
+                  <title>Space Force Portal</title>
+                  <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                  />
+                  <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                  />
+                  <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                  />
+                  <link
+                    rel="manifest"
+                    href="/site.webmanifest"
+                    crossOrigin="use-credentials"
+                  />
+                  <link
+                    rel="mask-icon"
+                    href="/safari-pinned-tab.svg"
+                    color="#5bbad5"
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-Book.woff"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-Book.woff2"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-BookItalic.woff"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-BookItalic.woff2"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-Medium.woff"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-Medium.woff2"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-MediumItalic.woff"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-MediumItalic.woff2"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-Bold.woff"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-Bold.woff2"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-BoldItalic.woff"
+                    as="font"
+                    crossOrigin=""
+                  />
+                  <link
+                    rel="preload"
+                    href="/assets/fonts/SharpSans-BoldItalic.woff2"
+                    as="font"
+                    crossOrigin=""
+                  />
 
-                <meta name="msapplication-TileColor" content="#da532c" />
-                <meta name="theme-color" content="#ffffff" />
-              </Head>
-              {getLayout(<Component {...pageProps} />)}
-            </ModalProvider>
-          </ThemeProvider>
+                  <meta name="msapplication-TileColor" content="#da532c" />
+                  <meta name="theme-color" content="#ffffff" />
+                </Head>
+                {getLayout(<Component {...pageProps} />)}
+              </ModalProvider>
+            </ThemeProvider>
+          </LaunchDarkly>
         </AuthProvider>
       </AnalyticsProvider>
     </ApolloProvider>
@@ -181,6 +183,4 @@ USSFPortalApp.getInitialProps = async (appContext: AppContext) => {
   return { ...appProps, hostname }
 }
 
-export default withLDProvider({
-  clientSideID: process.env.LAUNCHDARKLY_SDK_CLIENT_SIDE_ID!,
-})(USSFPortalApp as unknown as ComponentType)
+export default USSFPortalApp
