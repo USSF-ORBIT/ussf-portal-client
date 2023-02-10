fd850ef00fad3816bc04110de6fb9dfe9bcabe12
John Gedeon <john@truss.works>
2023-02-10 09:34:47 -0800

    refactor: update LinkTo to use new Link format
    eventually we should remove LinkTo and replace it with direct calls to
    Link

--- src/components/util/LinkTo/LinkTo.tsx
+++ src/components/util/LinkTo/LinkTo.tsx
@@ -10,23 +10,10 @@ export type PropTypes = LinkProps &
 /// A unified component for the next/link <Link> and a standard <a> anchor.
 /// Will lift href and all other props from Link up to the Link.
 /// Will automatically make an <a> tag containing the children and pass it remaining props.
-const LinkTo = ({
-  children,
-  href,
-  as,
-  replace,
-  scroll,
-  shallow,
-  prefetch,
-  locale,
-  ...anchorProps
-}: PropsWithChildren<PropTypes>) => {
+const LinkTo = ({ children, ...props }: PropsWithChildren<PropTypes>) => {
   return (
     // These props are lifted up to the `Link` element. All others are passed to the `<a>`
-    <Link {...{ href, as, replace, scroll, shallow, prefetch, locale }}>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <a {...anchorProps}>{children}</a>
-    </Link>
+    <Link {...props}>{children}</Link>
   )
 }
 
