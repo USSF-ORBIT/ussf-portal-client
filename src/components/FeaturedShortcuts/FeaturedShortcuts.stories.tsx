29ac23149ea05c43899094f7b9b92326a60b3c8e
Shauna Keating <shauna@truss.works>
2023-03-23 16:49:24 -0400

    adds mocked object id to featured shortcuts story

--- src/components/FeaturedShortcuts/FeaturedShortcuts.stories.tsx
+++ src/components/FeaturedShortcuts/FeaturedShortcuts.stories.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { Meta } from '@storybook/react'
+import { ObjectId } from 'bson'
 import FeaturedShortcuts from './FeaturedShorcuts'
 import { featuredShortcutItems } from './FeaturedShortcutItems'
+import { Widget } from 'types/index'
+
+const mockFeaturedShortcutsWidget: Widget = {
+  _id: new ObjectId(),
+  title: 'Featured Shortcuts',
+  type: 'FeaturedShortcuts',
+}
 
 export default {
   title: 'Components/Widgets/FeaturedShortcuts',
   component: FeaturedShortcuts,
+  decorators: [
+    (Story) => (
+      <div className="sfds">
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta
 
 export const FeaturedShorcutsWidget = () => (
-  <FeaturedShortcuts featuredShortcuts={featuredShortcutItems} widget />
+  <FeaturedShortcuts
+    featuredShortcuts={featuredShortcutItems}
+    widget={mockFeaturedShortcutsWidget}
+  />
 )
