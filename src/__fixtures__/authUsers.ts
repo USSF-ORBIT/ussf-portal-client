2cec41768bb5cdea9313ba49090e531416857ae7
Jacob Capps <jcapps@truss.works>
2023-03-28 10:39:21 -0500

    Update mock data to fix storybook issue

--- src/__fixtures__/authUsers.ts
+++ src/__fixtures__/authUsers.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from 'mongodb'
+import { ObjectId } from 'bson'
 import type { SessionUser, PortalUser } from 'types'
 
 export const testUser1: SessionUser = {
@@ -25,37 +25,37 @@ export const testPortalUser1: PortalUser = {
   userId: 'BERNADETTE.CAMPBELL.5244446289@testusers.cce.af.mil',
   mySpace: [
     {
-      _id: ObjectId(),
+      _id: new ObjectId('641dee649934af1088164f20'),
       cmsId: 'ckwz3u58s1835ql974leo1yll',
       title: 'Example Collection',
       type: 'Collection',
       bookmarks: [
         {
-          _id: ObjectId(),
+          _id: new ObjectId('641dee649934af1088164f21'),
           cmsId: 'cktd7c0d30190w597qoftevq1',
           url: 'https://afpcsecure.us.af.mil/',
           label: 'vMPF',
         },
         {
-          _id: ObjectId(),
+          _id: new ObjectId('641dee649934af1088164f22'),
           cmsId: 'cktd7ettn0457w597p7ja4uye',
           url: 'https://leave.af.mil/profile',
           label: 'LeaveWeb',
         },
         {
-          _id: ObjectId(),
+          _id: new ObjectId('641dee649934af1088164f23'),
           cmsId: 'cktd7hjz30636w5977vu4la4c',
           url: 'https://mypay.dfas.mil/#/',
           label: 'MyPay',
         },
         {
-          _id: ObjectId(),
+          _id: new ObjectId('641dee649934af1088164f24'),
           cmsId: 'ckwz3tphw1763ql97pia1zkvc',
           url: 'https://webmail.apps.mil/',
           label: 'Webmail',
         },
         {
-          _id: ObjectId(),
+          _id: new ObjectId('641dee649934af1088164f25'),
           cmsId: 'ckwz3u4461813ql970wkd254m',
           url: 'https://www.e-publishing.af.mil/',
           label: 'e-Publications',
