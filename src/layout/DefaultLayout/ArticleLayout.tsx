083d94a40f1d4e3f06c80f63e2a6bcbe02209211
Jacob Capps <jcapps@truss.works>
2023-03-28 10:58:37 -0500

    Add setMongoUserInfo to additional layouts

--- src/layout/DefaultLayout/ArticleLayout.tsx
+++ src/layout/DefaultLayout/ArticleLayout.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { GovBanner } from '@trussworks/react-uswds'
-
+import { useTheme } from 'next-themes'
 import styles from './DefaultLayout.module.scss'
-
 import Header from 'components/Header/Header'
 import Footer from 'components/Footer/Footer'
+import { useGetUserQuery } from 'operations/portal/queries/getUser.g'
+import { useAuthContext } from 'stores/authContext'
+import { PortalUser } from 'types'
 
 const ArticleLayout = ({ children }: { children: React.ReactNode }) => {
+  const { setMongoUserInfo } = useAuthContext()
+  const { setTheme } = useTheme()
+
+  const { data }: PortalUser | any = useGetUserQuery()
+
+  useEffect(() => {
+    setMongoUserInfo(data)
+    if (data) {
+      setTheme(data.theme)
+    }
+  }, [data])
+
   return (
     <>
       <a className="usa-skipnav" href="#main-content">
