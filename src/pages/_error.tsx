63e5dbd0fdcf083485810a5a087f51fa20fcfe1c
Shauna Keating <shauna@truss.works>
2023-04-06 22:42:32 -0400

    adds the same page content on _error.tsx

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -20,6 +20,7 @@ const Error: NextPageWithLayout<Props> = ({ statusCode }: Props) => {
 
   return (
     <>
+      <h1>500</h1>
       <GridContainer>
         <section>
           <Logo noText />
@@ -27,20 +28,34 @@ const Error: NextPageWithLayout<Props> = ({ statusCode }: Props) => {
           <h2>Houston, we have a problem</h2>
 
           <h3>
-            That’s {statusCode ? `an internal server error` : 'a client error'}.
-            While we work on fixing that, let’s get you back to business.
+            That’s an internal server error. While we work on fixing that, let’s
+            get you back to business. You may also submit a report to us at{' '}
+            <a
+              href={`mailto:${FEEDBACK_EMAIL}?subject=${FEEDBACK_SUBJECT}`}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="usa-link"
+              onClick={() => trackEvent('Feedback', FEEDBACK_EMAIL)}>
+              {FEEDBACK_EMAIL}
+            </a>{' '}
+            to tell us about what happened that brought you here.
           </h3>
 
-          <Button type="button" secondary onClick={handleBackClick}>
-            Return to previous page
-          </Button>
+          <div className="flex-align-center">
+            <Button type="button" secondary onClick={handleBackClick}>
+              Return to previous page
+            </Button>
+            <a
+              href={`mailto:${FEEDBACK_EMAIL}?subject=${FEEDBACK_SUBJECT}`}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="usa-button usa-button--outline usa-button--inverse"
+              onClick={() => trackEvent('Feedback', FEEDBACK_EMAIL)}>
+              Report a bug
+            </a>
+          </div>
         </section>
       </GridContainer>
-      {statusCode && (
-        <div style={{ overflow: 'hidden' }}>
-          <h1>{statusCode}</h1>
-        </div>
-      )}
     </>
   )
 }
