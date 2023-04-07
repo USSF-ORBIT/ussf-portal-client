e7e6d94f5bdce4d394ca5ccfad8adef5013d45a2
Shauna Keating <shauna@truss.works>
2023-04-06 21:00:31 -0400

    refactors 404 page to include bug report email

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { GridContainer } from '@trussworks/react-uswds'
+import { ButtonGroup, GridContainer } from '@trussworks/react-uswds'
 
 import { useUser } from 'hooks/useUser'
 
@@ -6,6 +6,11 @@ import Loader from 'components/Loader/Loader'
 import { withErrorLayout } from 'layout/ErrorLayout/ErrorLayout'
 import LinkTo from 'components/util/LinkTo/LinkTo'
 import Logo from 'components/Logo/Logo'
+
+import { useAnalytics } from 'stores/analyticsContext'
+
+const FEEDBACK_EMAIL = 'feedback@ussforbit.us'
+const FEEDBACK_SUBJECT = 'USSF portal feedback -- 404 page error'
 
 export default function Custom404() {
   const { user } = useUser()
@@ -20,14 +25,24 @@ export default function Custom404() {
 
           <h2>Looks like you’re a little lost</h2>
 
-          <h3>
+          <h3 className="width-tablet">
             That page doesn’t exist (or never did). Let’s get you back where you
-            belong.
+            belong, or send us a dispatch to notify us of an error in the
+            system.
           </h3>
-
-          <LinkTo className="usa-button usa-button--secondary" href="/">
-            Take me home
-          </LinkTo>
+          <div className="flex-align-center">
+            <LinkTo className="usa-button usa-button--secondary" href="/">
+              Take me home
+            </LinkTo>
+            <a
+              href={`mailto:${FEEDBACK_EMAIL}?subject=${FEEDBACK_SUBJECT}`}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="usa-button usa-button--outline usa-button--inverse"
+              onClick={() => trackEvent('Feedback', FEEDBACK_EMAIL)}>
+              {FEEDBACK_EMAIL}
+            </a>
+          </div>
         </section>
       </GridContainer>
       <div style={{ overflow: 'hidden' }}>
