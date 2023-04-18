0ea95f35d7a96e29e78a5538d7eef6fded9339e1
minh <minhlai@users.noreply.github.com>
2023-04-17 22:57:16 -0400

    build: download ca bundle without strict cert check and verify sha256 sum (#996)
    * build: download ca bundle without strict cert check and verify sha256 sum
    
    * store sha sums in gh secrets
    
    * make echos more secure
    
    * spelled certificate the most wrong anyone has ever misspelled it
    
    * add cert bundle sha to dev aws deploy workflow

--- scripts/create-gcds-chain.sh
+++ scripts/create-gcds-chain.sh
@@ -3,16 +3,16 @@
 cd /usr/local/share/ca-certificates
 
 # Make Dev GCDS PEM
-for cert in "/usr/local/share/ca-certificates/DoD_Root_CA_3.crt" "/usr/local/share/ca-certificates/DOD_EMAIL_CA-49.crt" "/usr/local/share/ca-certificates/federation.dev.cce.af.mil.crt"; do
+for cert in "/usr/local/share/ca-certificates/DoD_Root_CA_3.crt" "/usr/local/share/ca-certificates/DOD_EMAIL_CA-59.crt" "/usr/local/share/ca-certificates/federation.dev.cce.af.mil.crt"; do
     (cat "${cert}") >> Dev-GCDS.pem;
 done
 
 # Make Test GCDS PEM
-for cert in "/usr/local/share/ca-certificates/DoD_Root_CA_3.crt" "/usr/local/share/ca-certificates/DOD_EMAIL_CA-49.crt" "/usr/local/share/ca-certificates/federation.test.cce.af.mil.crt"; do
+for cert in "/usr/local/share/ca-certificates/DoD_Root_CA_3.crt" "/usr/local/share/ca-certificates/DOD_EMAIL_CA-59.crt" "/usr/local/share/ca-certificates/federation.test.cce.af.mil.crt"; do
     (cat "${cert}") >> Test-GCDS.pem;
 done
 
 # Make Prod GCDS PEM
-for cert in "/usr/local/share/ca-certificates/DoD_Root_CA_3.crt" "/usr/local/share/ca-certificates/DOD_EMAIL_CA-49.crt" "/usr/local/share/ca-certificates/federation.prod.cce.af.mil.crt"; do
+for cert in "/usr/local/share/ca-certificates/DoD_Root_CA_3.crt" "/usr/local/share/ca-certificates/DOD_EMAIL_CA-59.crt" "/usr/local/share/ca-certificates/federation.prod.cce.af.mil.crt"; do
     (cat "${cert}") >> Prod-GCDS.pem;
 done
