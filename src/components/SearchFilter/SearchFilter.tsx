6e9dd0e7a7134515b5b5d3362ec5d3567b65fc51
Jacob Capps <jcapps@truss.works>
2023-05-16 09:22:50 -0500

    Add function to build queries

--- src/components/SearchFilter/SearchFilter.tsx
+++ src/components/SearchFilter/SearchFilter.tsx
@@ -10,7 +10,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './SearchFilter.module.scss'
 
 const SearchFilter = () => {
+  // QUERY BUILDING:
+  // category: application, news, documentation
+  // label: label-1, label-2, label-3
+  // tag: tag-1, tag-2, tag-3
   const [query, setQuery] = useState<string>('')
+
+  // We are building a query to filter search on. This function runs each time any of the inputs
+  // are changed. It looks at the current query and splits it into an array. It checks if the value
+  // is already in the array. If it is, it removes it. If it isn't, it
+  // adds it. Then it sets the query to the new array joined by spaces.
+  const updateQuery = (queryValue: string) => {
+    const queryArray = query.split(' ')
+    if (queryArray.includes(queryValue)) {
+      const index = queryArray.indexOf(queryValue)
+      queryArray.splice(index, 1)
+      setQuery(queryArray.join(' '))
+    } else {
+      setQuery(query + ' ' + queryValue)
+    }
+  }
 
   return (
     <section className={styles.searchFilter}>
@@ -30,24 +49,31 @@ const SearchFilter = () => {
             label="Application"
             value="application"
             className={styles.checkbox}
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={(e) => updateQuery(`category:${e.target.value}`)}
           />
           <Checkbox
             id="news-filter"
             name="news"
             label="News"
+            value="news"
             className={styles.checkbox}
+            onChange={(e) => updateQuery(`category:${e.target.value}`)}
           />
           <Checkbox
             id="documentation-filter"
             name="documentation"
             label="Documentation"
+            value="documentation"
             className={styles.checkbox}
+            onChange={(e) => updateQuery(`category:${e.target.value}`)}
           />
         </Grid>
         <Grid col="auto">
           <h5 className={styles.subHeader}>Labels:</h5>
-          <Dropdown id="label-dropdown" name="label-dropdown">
+          <Dropdown
+            id="label-dropdown"
+            name="label-dropdown"
+            onChange={(e) => updateQuery(`label:${e.target.value}`)}>
             <option>None applied</option>
             <option value="label-1">Label 1</option>
             <option value="label-2">Label 2</option>
