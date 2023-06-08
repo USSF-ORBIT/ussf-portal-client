c73934b806f0e900a53648365843b73e75f4f341
Jacob Capps <jcapps@truss.works>
2023-06-08 14:56:01 -0500

    Remove comment

--- migrations/1686078533740-remove-duplicate-users.js
+++ migrations/1686078533740-remove-duplicate-users.js
@@ -4,10 +4,6 @@
 const { runMigration } = require('../utils/mongodb')
 
 module.exports.up = runMigration(async (db) => {
-  // Find any duplicate users using the userId field. If there are duplicates, merge the mySpace arrays
-  // into the mySpace array of the first user. If any of the duplicate users have a theme with the value
-  // 'dark', set the theme of the first user to 'dark'. Keep the displayName of the first user. Delete
-  // the duplicate users.
   const users = await db.collection('users').find({}).toArray()
   const userIds = users.map((user) => user.userId)
   const uniqueUserIds = [...new Set(userIds)]
