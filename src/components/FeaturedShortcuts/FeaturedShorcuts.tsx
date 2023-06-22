3cc8b62fa3e2b2b538f59897d6c1dd000d77e0c3
Shauna Keating <shauna@truss.works>
2023-06-22 12:54:58 -0400

    adds open in new window text to feautured shortcuts

--- src/components/FeaturedShortcuts/FeaturedShorcuts.tsx
+++ src/components/FeaturedShortcuts/FeaturedShorcuts.tsx
@@ -71,6 +71,7 @@ const FeaturedShortcuts = ({
                 data-testid="featured-shortcut-link">
                 <img src={a.icon} alt="" />
                 {a.title}
+                <span className="usa-sr-only">(opens in a new window)</span>
               </LinkTo>
             </li>
           )
