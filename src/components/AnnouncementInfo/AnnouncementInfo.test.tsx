e9a2459455d244956ec77c1d6c986809b585e827
John Gedeon <john@truss.works>
2023-06-30 12:04:32 -0700

    fix: handle removed articles linked in announcements and missing articles for cms users (#1057)
    * fix: announcement with CTA to deleted article links to 404
    
    * fix: return 404 if article not found and cms user

--- src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
+++ src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
@@ -9,31 +9,45 @@ import AnnouncementInfo from './AnnouncementInfo'
 import {
   testAnnouncementWithArticle,
   testAnnouncementWithArticleNoSlug,
+  testAnnouncementWithDeletedArticle,
   testAnnouncementWithUrl,
 } from '__fixtures__/data/cmsAnnouncments'
 
 describe('AnnouncementInfo component', () => {
-  it('renders an announcement with a url CTA', () => {
+  test('renders an announcement with a url CTA', () => {
     render(<AnnouncementInfo announcement={testAnnouncementWithUrl} />)
 
     expect(screen.getAllByText('Test Announcement')).toHaveLength(1)
     expect(screen.getAllByText('View more')).toHaveLength(1)
   })
 
-  it('renders an announcement with a default url if none is provided', () => {
+  test('renders an announcement with a default url if none is provided', () => {
     render(
       <AnnouncementInfo announcement={testAnnouncementWithArticleNoSlug} />
     )
     expect(screen.getAllByText('Cool new article')).toHaveLength(1)
 
     const link = screen.getByRole('link', { name: 'View article' })
-    expect(link).toHaveAttribute('href', '/')
+    expect(link).toHaveAttribute('href', '/404')
   })
 
-  it('renders an announcement with an article CTA', () => {
+  test('renders an announcement with an article CTA', () => {
     render(<AnnouncementInfo announcement={testAnnouncementWithArticle} />)
 
     expect(screen.getAllByText('Cool new article')).toHaveLength(1)
     expect(screen.getAllByText('View article')).toHaveLength(1)
+    const link = screen.getByRole('link', { name: 'View article' })
+    expect(link).toHaveAttribute('href', '/articles/something')
+  })
+
+  test('renders an announcement with a missing article CTA', () => {
+    render(
+      <AnnouncementInfo announcement={testAnnouncementWithDeletedArticle} />
+    )
+
+    expect(screen.getAllByText('Cool new deleted article')).toHaveLength(1)
+    expect(screen.getAllByText('View deleted article')).toHaveLength(1)
+    const link = screen.getByRole('link', { name: 'View deleted article' })
+    expect(link).toHaveAttribute('href', '/404')
   })
 })
