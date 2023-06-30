006dc00b5663f67a82b899e7dbaa3b1f9ec2209d
John Gedeon <john@truss.works>
2023-06-29 21:42:06 -0700

    fix storybook upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,12 @@
 const path = require('path')
 
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y',
     'storybook-addon-apollo-client',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    'storybook-css-modules-preset',
-    'storybook-addon-next-router',
     'storybook-theme-toggle',
     'storybook-addon-mock',
     'storybook-addon-launchdarkly',
@@ -25,25 +20,8 @@ module.exports = {
     },
   },
   staticDirs: ['../public'],
+
   webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      include: path.resolve(__dirname, '../'),
-      use: [
-        'style-loader',
-
-        // All CSS assets will be served from /public so no need to resolve URLs
-        { loader: 'css-loader', options: { url: false } },
-
-        {
-          loader: 'sass-loader',
-          options: {
-            warnRuleAsWarning: false,
-          },
-        },
-      ],
-    })
-    //#TODO does this need to be changed in order to import the uswds-core package in scss files?
     config.resolve.alias.uswds = path.resolve(
       __dirname,
       '../node_modules/@uswds/uswds/packages'
@@ -52,7 +30,14 @@ module.exports = {
     config.resolve.modules.push(path.resolve(__dirname, '../src'))
     config.resolve.modules.push('node_modules')
 
-    // Return the altered config
     return config
+  },
+
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 }
