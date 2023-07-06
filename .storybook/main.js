36bdfe4a7c1f115325f54923920f7dc48289b3f3
Abigail Young <abbyoung@gmail.com>
2023-07-06 09:33:22 -0700

    chore(deps): Upgrade USWDS, React USWDS, and Storybook (#1058)
    * Signing WIP commit
    
    * Save point for version that compiles. Some missing styles but looking good
    
    * fix a couple compile errors
    
    * add back the postinstall we need to copy vendor files
    
    * case sensitivity in test somehow got out of sync
    
    * fix ordering of stylesheets to correct discrepancies
    
    * cleaning up wip files
    
    * more cleanup for consistency
    
    * remove unused var theme-hero-image
    
    * chore(deps): update storybook monorepo to v7
    
    * fix storybook upgrade
    
    * update storybook again
    
    * disable storybook auto doc gen
    
    * fix strange error in modalContext tests
    
    * split storybook build from happo build
    
    * upgrade to latest storybook
    
    * use shortened path for index.scss file
    
    * update comment
    
    * moved styles that resolve duplicate alert icon issue into storybook only stylesheet
    
    * adjusts css to work on storybook and in production to hide random extra icon
    
    * adds slim attribute to stories for alerts to match what they used to look like
    
    * remove deprecated file
    
    * add missing import statements
    
    * workaround for prefixing css in storybook
    
    * putting back initIcons because we need those!
    
    * remove unneccesary comments
    
    ---------
    
    Co-authored-by: John Gedeon <john@truss.works>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Shauna Keating <shauna@truss.works>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,12 @@
 const path = require('path')
 
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y',
     'storybook-addon-apollo-client',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    'storybook-css-modules-preset',
-    'storybook-addon-next-router',
     'storybook-theme-toggle',
     'storybook-addon-mock',
     'storybook-addon-launchdarkly',
@@ -25,28 +20,25 @@ module.exports = {
     },
   },
   staticDirs: ['../public'],
+
   webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-
-        // All CSS assets will be served from /public so no need to resolve URLs
-        { loader: 'css-loader', options: { url: false } },
-
-        { loader: 'sass-loader', options: { warnRuleAsWarning: false } },
-      ],
-    })
-
     config.resolve.alias.uswds = path.resolve(
       __dirname,
-      '../node_modules/uswds'
+      '../node_modules/@uswds/uswds/packages'
     )
     config.resolve.modules = config.resolve.modules || []
     config.resolve.modules.push(path.resolve(__dirname, '../src'))
     config.resolve.modules.push('node_modules')
 
-    // Return the altered config
     return config
+  },
+
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+
+  docs: {
+    autodocs: 'tag',
   },
 }
