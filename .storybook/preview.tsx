e6f796a181fdad1c371b9cebc0a6c5db17df5ea4
John Gedeon <john@truss.works>
2023-07-14 16:42:36 -0700

    remove unneeded comments

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -136,12 +136,9 @@ const preview: Preview = {
       description: 'Global theme for components',
       defaultValue: 'light',
       toolbar: {
-        // The label to show for this toolbar item
         title: 'Theme',
         icon: 'circlehollow',
-        // Array of plain string values or MenuItem shape (see below)
         items: ['light', 'dark'],
-        // Change title based on selected value
         dynamicTitle: true,
       },
     },
