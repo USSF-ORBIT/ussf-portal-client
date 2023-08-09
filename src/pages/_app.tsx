a67b340379aaeb471c3cceb508a1bbb533c7f338
John Gedeon <john@truss.works>
2023-08-09 13:51:51 -0700

    auth needs to call analytics on log in and out

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -49,8 +49,8 @@ const USSFPortalApp = ({ Component, pageProps, hostname }: Props) => {
 
   return (
     <ApolloProvider client={client}>
-      <AuthProvider>
-        <AnalyticsProvider>
+      <AnalyticsProvider>
+        <AuthProvider>
           <MySpaceProvider>
             <SearchProvider>
               <LaunchDarkly>
@@ -181,8 +181,8 @@ const USSFPortalApp = ({ Component, pageProps, hostname }: Props) => {
               </LaunchDarkly>
             </SearchProvider>
           </MySpaceProvider>
-        </AnalyticsProvider>
-      </AuthProvider>
+        </AuthProvider>
+      </AnalyticsProvider>
     </ApolloProvider>
   )
 }
