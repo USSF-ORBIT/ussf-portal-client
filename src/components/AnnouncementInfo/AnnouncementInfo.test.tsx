6b44680fa1f681d3f9534532f49994e981747c1b
Abigail Young <abbyoung@gmail.com>
2023-08-22 11:42:11 -0700

    update unit tests for announcement info

--- src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
+++ src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
@@ -5,22 +5,21 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 
 import React from 'react'
-
 import AnnouncementInfo from './AnnouncementInfo'
+import * as openDocumentLink from 'helpers/openDocumentLink'
 import {
   testAnnouncementWithArticle,
   testAnnouncementWithArticleNoSlug,
   testAnnouncementWithDeletedArticle,
   testAnnouncementWithUrl,
-  testAnnouncementWithDocument,
+  testAnnouncementWithPdfDocument,
+  testAnnouncementWithJpgDocument,
 } from '__fixtures__/data/cmsAnnouncments'
-import { handleOpenPdfLink } from 'helpers/openDocumentLink'
 
-// Mock the function that opens PDFs in the browser
-jest.mock('helpers/openDocumentLink', () => ({
-  isPdf: jest.fn(),
-  handleOpenPdfLink: jest.fn(),
-}))
+// Spy on the functions that test files and opens PDFs in the browser
+// We are not mocking because we want to test isPdf
+jest.spyOn(openDocumentLink, 'handleOpenPdfLink').mockImplementation()
+jest.spyOn(openDocumentLink, 'isPdf')
 
 describe('AnnouncementInfo component', () => {
   test('renders an announcement with a url CTA', () => {
@@ -60,12 +59,12 @@ describe('AnnouncementInfo component', () => {
     expect(link).toHaveAttribute('href', '/404')
   })
 
-  test('renders an announcement with a document CTA', async () => {
+  test('renders an announcement with a pdf document CTA', async () => {
     const pdfString =
-      testAnnouncementWithDocument.body.document[1].props?.link?.value?.data
+      testAnnouncementWithPdfDocument.body.document[1].props?.link?.value?.data
         ?.file?.url
 
-    render(<AnnouncementInfo announcement={testAnnouncementWithDocument} />)
+    render(<AnnouncementInfo announcement={testAnnouncementWithPdfDocument} />)
 
     expect(screen.getAllByText('Test Announcement Document')).toHaveLength(1)
     expect(screen.getAllByText('Read more')).toHaveLength(1)
@@ -74,7 +73,33 @@ describe('AnnouncementInfo component', () => {
     expect(link).toHaveAttribute('href', pdfString)
 
     fireEvent.click(link)
-    expect(handleOpenPdfLink).toHaveBeenCalledTimes(1)
-    expect(handleOpenPdfLink).toHaveBeenCalledWith(pdfString)
+    expect(openDocumentLink.isPdf).toHaveBeenCalledTimes(1)
+    expect(openDocumentLink.isPdf).toHaveBeenCalledWith(pdfString)
+    expect(openDocumentLink.handleOpenPdfLink).toHaveBeenCalledTimes(1)
+    expect(openDocumentLink.handleOpenPdfLink).toHaveBeenCalledWith(pdfString)
+  })
+
+  test('renders an announcement with a non-pdf document CTA', async () => {
+    jest.resetAllMocks()
+
+    const jpgString =
+      testAnnouncementWithJpgDocument.body.document[1].props?.link?.value?.data
+        ?.file?.url
+
+    render(<AnnouncementInfo announcement={testAnnouncementWithJpgDocument} />)
+
+    expect(screen.getAllByText('Test Announcement Jpg Document')).toHaveLength(
+      1
+    )
+    expect(screen.getAllByText('Read more')).toHaveLength(1)
+
+    const link = screen.getByRole('link', { name: 'Read more' })
+    expect(link).toHaveAttribute('href', jpgString)
+
+    fireEvent.click(link)
+    expect(openDocumentLink.isPdf).toHaveBeenCalledTimes(1)
+    expect(openDocumentLink.isPdf).toHaveBeenCalledWith(jpgString)
+
+    expect(openDocumentLink.handleOpenPdfLink).toHaveBeenCalledTimes(0)
   })
 })
