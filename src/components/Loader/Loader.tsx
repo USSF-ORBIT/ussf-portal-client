cabc24a2cbabe555cac2b411ce84bc9276035b22
Shauna Keating <shauna@truss.works>
2023-08-24 15:14:52 -0400

    removes old loader code

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -6,14 +6,7 @@ const Loader = () => (
   <div className={styles.loader}>
     <p className="usa-sr-only">Content is loading...</p>
 
-    <div className={styles.container}>
-      <div className={styles.planet}>
-        <img src="/assets/images/earth.svg" alt=" " />
-      </div>
-      <div className={styles.sate}>
-        <div></div>
-      </div>
-    </div>
+    <div className={styles.container}></div>
   </div>
 )
 
