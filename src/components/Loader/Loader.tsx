ed8bec6a38c52ca00f67eebc94bad40ec93af732
Shauna Keating <shauna@truss.works>
2023-08-24 16:52:31 -0400

    adds logo

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -6,7 +6,12 @@ const Loader = () => (
   <div className={styles.loader}>
     <p className="usa-sr-only">Content is loading...</p>
 
-    <div className={styles.container}></div>
+    <div className={styles.container}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/United_States_Space_Force_logo.svg/1200px-United_States_Space_Force_logo.svg.png"
+        alt=" "
+      />
+    </div>
   </div>
 )
 
