fa0a6bb3b364576923782f428fd35007ca83aff9
Abigail Young <abbyoung@gmail.com>
2023-08-24 12:28:03 -0700

    feat: Add support for CTA to link a document (#1082)
    * add support for document cta and open pdfs in browser
    
    * small refactor and clean up tests
    
    * fix axios call to get blob
    
    * bug fixes and linter cleanup
    
    * use var to make it easier to read
    
    * remove unused test
    
    * update unit tests for announcement info
    
    * prevent undefined if error in cms validation
    
    * remove old comment
    
    ---------
    
    Co-authored-by: Jacob Capps <99674188+jcbcapps@users.noreply.github.com>

--- src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
+++ src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
@@ -2,16 +2,24 @@
  * @jest-environment jsdom
  */
 
-import { render, screen } from '@testing-library/react'
-import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
+import React from 'react'
 import AnnouncementInfo from './AnnouncementInfo'
+import * as openDocumentLink from 'helpers/openDocumentLink'
 import {
   testAnnouncementWithArticle,
   testAnnouncementWithArticleNoSlug,
   testAnnouncementWithDeletedArticle,
   testAnnouncementWithUrl,
+  testAnnouncementWithPdfDocument,
+  testAnnouncementWithJpgDocument,
 } from '__fixtures__/data/cmsAnnouncments'
+
+// Spy on the functions that test files and opens PDFs in the browser
+// We are not mocking because we want to test isPdf
+jest.spyOn(openDocumentLink, 'handleOpenPdfLink').mockImplementation()
+jest.spyOn(openDocumentLink, 'isPdf')
 
 describe('AnnouncementInfo component', () => {
   test('renders an announcement with a url CTA', () => {
@@ -49,5 +57,49 @@ describe('AnnouncementInfo component', () => {
     expect(screen.getAllByText('View deleted article')).toHaveLength(1)
     const link = screen.getByRole('link', { name: 'View deleted article' })
     expect(link).toHaveAttribute('href', '/404')
+  })
+
+  test('renders an announcement with a pdf document CTA', async () => {
+    const pdfString =
+      testAnnouncementWithPdfDocument.body.document[1].props?.link?.value?.data
+        ?.file?.url
+
+    render(<AnnouncementInfo announcement={testAnnouncementWithPdfDocument} />)
+
+    expect(screen.getAllByText('Test Announcement Document')).toHaveLength(1)
+    expect(screen.getAllByText('Read more')).toHaveLength(1)
+
+    const link = screen.getByRole('link', { name: 'Read more' })
+    expect(link).toHaveAttribute('href', pdfString)
+
+    fireEvent.click(link)
+    expect(openDocumentLink.isPdf).toHaveBeenCalledTimes(1)
+    expect(openDocumentLink.isPdf).toHaveBeenCalledWith(pdfString)
+    expect(openDocumentLink.handleOpenPdfLink).toHaveBeenCalledTimes(1)
+    expect(openDocumentLink.handleOpenPdfLink).toHaveBeenCalledWith(pdfString)
+  })
+
+  test('renders an announcement with a non-pdf document CTA', async () => {
+    jest.resetAllMocks()
+
+    const jpgString =
+      testAnnouncementWithJpgDocument.body.document[1].props?.link?.value?.data
+        ?.file?.url
+
+    render(<AnnouncementInfo announcement={testAnnouncementWithJpgDocument} />)
+
+    expect(screen.getAllByText('Test Announcement Jpg Document')).toHaveLength(
+      1
+    )
+    expect(screen.getAllByText('Read more')).toHaveLength(1)
+
+    const link = screen.getByRole('link', { name: 'Read more' })
+    expect(link).toHaveAttribute('href', jpgString)
+
+    fireEvent.click(link)
+    expect(openDocumentLink.isPdf).toHaveBeenCalledTimes(1)
+    expect(openDocumentLink.isPdf).toHaveBeenCalledWith(jpgString)
+
+    expect(openDocumentLink.handleOpenPdfLink).toHaveBeenCalledTimes(0)
   })
 })
---
+++ src/helpers/openDocumentLink.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export const isPdf = (url: string) => {
+  return url.toString().endsWith('.pdf')
+}
+
+export const handleOpenPdfLink = async (pdfString: string) => {
+  // Fetch the file from Keystone / S3
+  const res = await axios.get(pdfString, { responseType: 'blob' })
+
+  // Create a blob from the file and open it in a new tab
+  const blobData = await res.data
+  const file = new Blob([blobData], { type: 'application/pdf' })
+  const fileUrl = URL.createObjectURL(file)
+
+  window.open(fileUrl)
+  // Let the browser know not to keep the reference to the file any longer.
+  URL.revokeObjectURL(fileUrl)
+}
