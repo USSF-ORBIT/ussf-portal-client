573e235823abd4928eb05dff2714972dd6291ff1
Shauna Keating <shauna@truss.works>
2023-08-25 13:42:05 -0400

    adds css stars and meteors

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -11,7 +11,28 @@ const Loader = () => (
         <img src="/assets/images/delta-stacked-light.png" alt=" " />
       </div>
       <div className={styles.back}>
-        <div className={styles.stars}></div>
+        <div className={styles.earth}>
+          <img src="/assets/images/hero-transparent.png" alt=" " />
+          <div className={styles.glow}> </div>
+        </div>
+        <div className={styles.cosmos}>
+          <div className={styles.star}></div>
+          <div className={styles.meteor1}></div>
+          <div className={styles.meteor2}></div>
+          <div className={styles.meteor3}></div>
+          <div className={styles.meteor4}></div>
+          <div className={styles.meteor5}></div>
+          <div className={styles.meteor6}></div>
+          <div className={styles.meteor7}></div>
+          <div className={styles.meteor8}></div>
+          <div className={styles.meteor9}></div>
+          <div className={styles.meteor10}></div>
+          <div className={styles.meteor11}></div>
+          <div className={styles.meteor12}></div>
+          <div className={styles.meteor13}></div>
+          <div className={styles.meteor14}></div>
+          <div className={styles.meteor15}></div>
+        </div>
       </div>
     </div>
   </div>
