1008ab8e56b9d3b8add0c8a11465bb73f24f73d3
Shauna Keating <shauna@truss.works>
2023-08-28 12:59:12 -0400

    setup of basic loader animation

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -14,21 +14,14 @@ const Loader = () => (
         <div className={styles.earth}></div>
         <div className={styles.cosmos}>
           <div className={styles.star}></div>
-          <div className={styles.meteor1}></div>
-          <div className={styles.meteor2}></div>
-          <div className={styles.meteor3}></div>
-          <div className={styles.meteor4}></div>
-          <div className={styles.meteor5}></div>
-          <div className={styles.meteor6}></div>
-          <div className={styles.meteor7}></div>
-          <div className={styles.meteor8}></div>
-          <div className={styles.meteor9}></div>
-          <div className={styles.meteor10}></div>
-          <div className={styles.meteor11}></div>
-          <div className={styles.meteor12}></div>
-          <div className={styles.meteor13}></div>
-          <div className={styles.meteor14}></div>
-          <div className={styles.meteor15}></div>
+          <div className={styles.spinner}>
+            <div className={styles.face}>
+              <div className={styles.circle}></div>
+            </div>
+            <div className={styles.face}>
+              <div className={styles.circle}></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
