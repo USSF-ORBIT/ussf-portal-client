c3384bb5b83cca12def5d9e89ffa11b9b44d84ac
Jacob Capps <jcapps@truss.works>
2023-09-11 12:36:59 -0500

    Uncomment

--- src/pages/api/dataSources/personnel.ts
+++ src/pages/api/dataSources/personnel.ts
@@ -8,7 +8,7 @@ class PersonnelAPI extends RESTDataSource {
 
   async getUserData(dodId: string) {
     // This sends a GraphQL query to the external Personnel API
-    // if (!this.baseURL) throw new Error('No Personnel API URL found')
+    if (!this.baseURL) throw new Error('No Personnel API URL found')
 
     return this.post(this.baseURL + `/api/graphql`, {
       query: `query GetUser($getUserId: String!) {
