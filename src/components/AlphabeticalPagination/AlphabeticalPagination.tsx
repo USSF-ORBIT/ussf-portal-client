5a87e77e37c302e44eea683733aa13d8ad21344d
Mallory Robertson <mallory@truss.works>
2023-09-13 11:21:00 -0700

    Sets up initial markup for pagination

--- src/components/AlphabeticalPagination/AlphabeticalPagination.tsx
+++ src/components/AlphabeticalPagination/AlphabeticalPagination.tsx
@@ -2,10 +2,95 @@
 
 import React from 'react'
 import styles from './AlphabeticalPagination.module.scss'
+import { Button, Icon, Link } from '@trussworks/react-uswds'
+
+type PaginationProps = {
+  pathname: string // pathname of results page
+  totalPages?: number // total items divided by items per page
+  currentPage: number // current page number (starting at 1)
+  maxSlots?: number // number of pagination "slots"
+  onClickNext?: () => void
+  onClickPrevious?: () => void
+  onClickPageNumber?: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    page: number
+  ) => void
+}
 
 const AlphabeticalPagination = () => (
   <div className={styles.alphapag}>
-    <nav className="usa-pagination"></nav>
+    <nav aria-label="Pagination" className="usa-pagination">
+      <ul className="usa-pagination__list">
+        <li className="usa-pagination__item usa-pagination__arrow">
+          <Button
+            type="button"
+            unstyled
+            className="usa-pagination__link usa-pagination__previous-page"
+            aria-label="Previous page"
+            data-testid="pagination-previous">
+            <Icon.NavigateBefore />
+            <span className="usa-pagination__link-text">Previous</span>
+          </Button>
+        </li>
+        <li className="usa-pagination__item usa-pagination__page-no">
+          <Link href="#" className="usa-pagination__button" aria-label="Page 1">
+            1
+          </Link>
+        </li>
+        <li
+          className="usa-pagination__item usa-pagination__overflow"
+          aria-label="ellipsis indicating non-visible pages">
+          <span>…</span>
+        </li>
+        <li className="usa-pagination__item usa-pagination__page-no">
+          <Link href="#" className="usa-pagination__button" aria-label="Page 9">
+            9
+          </Link>
+        </li>
+        <li className="usa-pagination__item usa-pagination__page-no">
+          <Link
+            href="#"
+            className="usa-pagination__button usa-current"
+            aria-label="Page 10"
+            aria-current="page">
+            10
+          </Link>
+        </li>
+        <li className="usa-pagination__item usa-pagination__page-no">
+          <Link
+            href="#"
+            className="usa-pagination__button"
+            aria-label="Page 11">
+            11
+          </Link>
+        </li>
+        <li
+          className="usa-pagination__item usa-pagination__overflow"
+          aria-label="ellipsis indicating non-visible pages">
+          <span>…</span>
+        </li>
+        <li className="usa-pagination__item usa-pagination__page-no">
+          <Link
+            href="#"
+            className="usa-pagination__button"
+            aria-label="Last page, page 24">
+            24
+          </Link>
+        </li>
+        <li className="usa-pagination__item usa-pagination__arrow">
+          <Button
+            type="button"
+            unstyled
+            className="usa-pagination__link usa-pagination__next-page"
+            aria-label="Next page"
+            data-testid="pagination-next">
+            <span className="usa-pagination__link-text">Next</span>
+            <Icon.NavigateNext />
+          </Button>
+        </li>
+      </ul>
+    </nav>
+
     {/* uswds uses quotes, not curlies */}
   </div>
 )
