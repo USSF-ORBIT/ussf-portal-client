b8cead1c5bc963f90ab8397b9d8412212386fbdf
Jacob Capps <jcapps@truss.works>
2023-09-27 11:42:55 -0500

    Disable lint error

--- migration-template.js
+++ migration-template.js
@@ -3,11 +3,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { runMigration } = require('../utils/mongodb')
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports.up = runMigration(async (db) => {
   // Up migration code here
   // await db...
 })
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports.down = runMigration(async (db) => {
   // Down migration code here
   // await db...
