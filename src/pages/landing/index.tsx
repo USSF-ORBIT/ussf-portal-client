72bf3f72713c0c4331daded092d7a13396df3a9a
Jacob Capps <jcapps@truss.works>
2023-11-01 11:12:20 -0500

    Add getLandingPages query and display results

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,9 +1,36 @@
+import { InferGetServerSidePropsType } from 'next'
+import { Grid } from '@trussworks/react-uswds'
+import Link from 'next/link'
 import { withLandingPageLayout } from 'layout/DefaultLayout/LandingPageLayout'
+import { GET_LANDING_PAGES } from 'operations/cms/queries/getLandingPages'
+import { client } from 'lib/keystoneClient'
 
-const Landing = () => {
+type LandingPage = {
+  pageTitle: string
+  slug: string
+}
+
+const Landing = ({
+  landingPages,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <div>
       <h1>Landing Pages</h1>
+      <div>
+        <Grid>
+          <ul>
+            {landingPages.map((landingPage: LandingPage) => {
+              return (
+                <li key={`landing_page_` + landingPage.slug}>
+                  <Link href={`/landing/${landingPage.slug}`}>
+                    {landingPage.pageTitle}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </Grid>
+      </div>
     </div>
   )
 }
@@ -11,3 +38,17 @@ const Landing = () => {
 export default Landing
 
 Landing.getLayout = (page: JSX.Element) => withLandingPageLayout(page)
+
+export async function getServerSideProps() {
+  const {
+    data: { landingPages },
+  } = await client.query({
+    query: GET_LANDING_PAGES,
+  })
+
+  return {
+    props: {
+      landingPages,
+    },
+  }
+}
