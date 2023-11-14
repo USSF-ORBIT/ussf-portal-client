2a279e2b87dd3a5f3103f247376cc64c85c2f631
Shauna Keating <shauna@truss.works>
2023-11-14 16:11:58 -0500

    scopes in page nav styles that should go to the component in the mixin, and whats specific to the landing page to that css module

--- src/stories/inpagenav.stories.tsx
+++ src/stories/inpagenav.stories.tsx
@@ -7,7 +7,7 @@ export default {
   title: 'Navigation/InPageNavigation',
 } as Meta
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line
 const pageContent = () => {
   return (
     <>
