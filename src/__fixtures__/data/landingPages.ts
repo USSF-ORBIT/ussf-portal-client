2cbfd981637e54ce5a1b67fbdc6f3193575a328a
Jacob Capps <jcapps@truss.works>
2023-11-14 09:46:51 -0600

    Add tests for landing page index

---
+++ src/__fixtures__/data/landingPages.ts
@@ -0,0 +1,12 @@
+export const mockLandingPages = [
+  {
+    pageDescription: 'This is a test landing page',
+    pageTitle: 'Test Landing Page',
+    slug: 'test-landing-page',
+  },
+  {
+    pageDescription: 'Another landing page',
+    pageTitle: 'Another Landing Page',
+    slug: 'another-landing-page',
+  },
+]
