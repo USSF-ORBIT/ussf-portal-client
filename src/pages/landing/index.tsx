a0ad9503644ac9f59326b306d2ce27b8e20a3256
Jacob Capps <jcapps@truss.works>
2023-11-27 14:23:33 -0600

    Move table into its own component

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,7 +1,5 @@
 import { InferGetServerSidePropsType } from 'next'
-import { Table } from '@trussworks/react-uswds'
-import Link from 'next/link'
-import styles from '../../styles/pages/landingIndex.module.scss'
+import LandingPageIndexTable from 'components/LandingPageIndexTable/LandingPageIndexTable'
 import { withDefaultLayout } from 'layout/DefaultLayout/DefaultLayout'
 import { GET_LANDING_PAGES } from 'operations/cms/queries/getLandingPages'
 import { client } from 'lib/keystoneClient'
@@ -21,26 +19,7 @@ const Landing = ({
   return (
     <div>
       <h1>Landing Pages</h1>
-      {sortedLandingPages.length > 0 && (
-        <Table
-          bordered
-          striped
-          className={`usa-table--borderless ${styles.table}`}>
-          <tbody>
-            {sortedLandingPages.map((landingPage: LandingPage) => {
-              return (
-                <tr key={`landing_page_` + landingPage.slug}>
-                  <td>
-                    <Link href={`/landing/${landingPage.slug}`}>
-                      {landingPage.pageTitle}
-                    </Link>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </Table>
-      )}
+      <LandingPageIndexTable landingPages={sortedLandingPages} />
     </div>
   )
 }
