5a20e90d64f8914f22f4cbb83b01e31f468c22ba
Shauna Keating <shauna@truss.works>
2023-12-12 00:55:53 -0500

    removes references to feedback card from layout and adds space above main to account for sticky header

--- src/layout/DefaultLayout/DefaultLayout.tsx
+++ src/layout/DefaultLayout/DefaultLayout.tsx
@@ -5,7 +5,6 @@ import Header from 'components/Header/Header'
 import PersonalData from 'components/PersonalData/PersonalData'
 import PageHeader from 'components/PageHeader/PageHeader'
 import PageNav from 'components/PageNav/PageNav'
-import FeedbackCard from 'components/FeedbackCard/FeedbackCard'
 import Footer from 'components/Footer/Footer'
 import CustomModal from 'components/CustomModal/CustomModal'
 import Loader from 'components/Loader/Loader'
@@ -15,11 +14,9 @@ import { useUser } from 'hooks/useUser'
 import { MySpace } from 'types'
 
 const DefaultLayout = ({
-  displayFeedbackCard = false,
   rightSidebar = undefined,
   children,
 }: {
-  displayFeedbackCard?: boolean
   rightSidebar?: JSX.Element
   children: React.ReactNode
 }) => {
@@ -62,16 +59,15 @@ const DefaultLayout = ({
           <GridContainer>
             <Grid row gap>
               {/* LEFT SIDEBAR */}
-              <Grid tablet={{ col: displayFeedbackCard ? 3 : 2 }}>
+              <Grid desktop={3}>
                 <PageNav />
-                {displayFeedbackCard && <FeedbackCard />}
               </Grid>
-              <Grid tablet={{ col: true }}>
+              <Grid desktop={{ col: true }}>
                 {/* PAGE CONTENT */}
                 {children}
               </Grid>
               {/* RIGHT SIDEBAR */}
-              {rightSidebar && <Grid tablet={{ col: 4 }}>{rightSidebar}</Grid>}
+              {rightSidebar && <Grid desktop={{ col: 4 }}>{rightSidebar}</Grid>}
             </Grid>
           </GridContainer>
         </main>
@@ -87,14 +83,7 @@ export default DefaultLayout
 
 export const withDefaultLayout = (
   page: React.ReactNode,
-  displayFeedbackCard?: boolean,
   rightSidebar?: JSX.Element
 ) => {
-  return (
-    <DefaultLayout
-      displayFeedbackCard={displayFeedbackCard}
-      rightSidebar={rightSidebar}>
-      {page}
-    </DefaultLayout>
-  )
+  return <DefaultLayout rightSidebar={rightSidebar}>{page}</DefaultLayout>
 }
