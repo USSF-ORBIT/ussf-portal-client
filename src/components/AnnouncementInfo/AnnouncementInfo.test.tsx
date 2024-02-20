09daa55776bdf5cf3663727e33ad85124bc859c4
Abigail Young <abbyoung@gmail.com>
2024-02-20 11:17:42 -0800

    update tests for additional mime types

--- src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
+++ src/components/AnnouncementInfo/AnnouncementInfo.test.tsx
@@ -16,10 +16,8 @@ import {
   testAnnouncementWithJpgDocument,
 } from '__fixtures__/data/cmsAnnouncments'
 
-// Spy on the functions that test files and opens PDFs in the browser
-// We are not mocking because we want to test isPdf
-jest.spyOn(openDocumentLink, 'handleOpenPdfLink').mockImplementation()
-jest.spyOn(openDocumentLink, 'isPdf')
+// Spy on the function that opens PDFs in the browser
+jest.spyOn(openDocumentLink, 'openFileInNewTab').mockImplementation()
 
 describe('AnnouncementInfo component', () => {
   test('renders an announcement with a url CTA', () => {
@@ -73,13 +71,11 @@ describe('AnnouncementInfo component', () => {
     expect(link).toHaveAttribute('href', pdfString)
 
     fireEvent.click(link)
-    expect(openDocumentLink.isPdf).toHaveBeenCalledTimes(1)
-    expect(openDocumentLink.isPdf).toHaveBeenCalledWith(pdfString)
-    expect(openDocumentLink.handleOpenPdfLink).toHaveBeenCalledTimes(1)
-    expect(openDocumentLink.handleOpenPdfLink).toHaveBeenCalledWith(pdfString)
+    expect(openDocumentLink.openFileInNewTab).toHaveBeenCalledTimes(1)
+    expect(openDocumentLink.openFileInNewTab).toHaveBeenCalledWith(pdfString)
   })
 
-  test('renders an announcement with a non-pdf document CTA', async () => {
+  test('renders an announcement with a jpg document CTA', async () => {
     jest.resetAllMocks()
 
     const jpgString =
@@ -97,9 +93,6 @@ describe('AnnouncementInfo component', () => {
     expect(link).toHaveAttribute('href', jpgString)
 
     fireEvent.click(link)
-    expect(openDocumentLink.isPdf).toHaveBeenCalledTimes(1)
-    expect(openDocumentLink.isPdf).toHaveBeenCalledWith(jpgString)
-
-    expect(openDocumentLink.handleOpenPdfLink).toHaveBeenCalledTimes(0)
+    expect(openDocumentLink.openFileInNewTab).toHaveBeenCalledTimes(1)
   })
 })
--- src/helpers/openDocumentLink.ts
+++ src/helpers/openDocumentLink.ts
@@ -1,28 +1,27 @@
 import axios from 'axios'
 
 // Helper function to determine MIME type from the Keystone file URL
-const getMimeType = (url: string): string => {
+export const mimeTypes: Record<string, string> = {
+  pdf: 'application/pdf',
+  jpg: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  png: 'image/png',
+  webp: 'image/webp',
+  gif: 'image/gif',
+  svg: 'image/svg+xml',
+  webm: 'video/webm',
+  mp4: 'video/mp4',
+  mov: 'video/quicktime',
+  mp3: 'audio/mpeg',
+  wav: 'audio/wav',
+  txt: 'text/plain',
+  csv: 'text/csv',
+  xls: 'application/vnd.ms-excel',
+  xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  // Add more mappings as needed
+}
+export const getMimeType = (url: string): string => {
   const extension = url.split('.').pop()?.toLowerCase()
-  const mimeTypes: Record<string, string> = {
-    pdf: 'application/pdf',
-    jpg: 'image/jpeg',
-    jpeg: 'image/jpeg',
-    png: 'image/png',
-    webp: 'image/webp',
-    gif: 'image/gif',
-    svg: 'image/svg+xml',
-    webm: 'video/webm',
-    mp4: 'video/mp4',
-    mov: 'video/quicktime',
-    mp3: 'audio/mpeg',
-    wav: 'audio/wav',
-    txt: 'text/plain',
-    csv: 'text/csv',
-    xls: 'application/vnd.ms-excel',
-    xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    // Add more mappings as needed
-  }
-
   return mimeTypes[extension || ''] || 'application/octet-stream' // Default MIME type
 }
 // Function to open files in a new tab with TypeScript annotations
