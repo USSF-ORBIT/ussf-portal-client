4f34bc4a5eb9337b6329cb52d72f69d08e4078c7
Jacob Capps <99674188+jcbcapps@users.noreply.github.com>
2024-02-21 14:09:34 -0600

    feat: Add Site Header (#1210)
    * Add query for site header
    
    * Update query
    
    * Add SiteHeader type
    
    * Add getSiteHeader to keystone datasource
    
    * Query for the site header in Header component
    
    * Display logout button if no site header
    
    * Update tests and remove some tests in favor of e2e tests
    
    * Update query
    
    * Update storybook

--- src/__fixtures__/operations/getTheme.ts
+++ src/__fixtures__/operations/getTheme.ts
@@ -1,16 +1,14 @@
 import { GetThemeDocument } from 'operations/portal/queries/getTheme.g'
 
-export const getThemeMock = [
-  {
-    request: {
-      query: GetThemeDocument,
-    },
-    result: jest.fn(
-      /* istanbul ignore next */ () => ({
-        data: {
-          theme: 'dark',
-        },
-      })
-    ),
+export const getThemeMock = {
+  request: {
+    query: GetThemeDocument,
   },
-]
+  result: jest.fn(
+    /* istanbul ignore next */ () => ({
+      data: {
+        theme: 'dark',
+      },
+    })
+  ),
+}
